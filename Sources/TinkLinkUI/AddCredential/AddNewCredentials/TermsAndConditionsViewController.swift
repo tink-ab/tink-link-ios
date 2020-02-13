ab51000cfda385a46fcd34b9f3a7858964735afe
Kasper Lahti <kasper.lahti@tink.se>
2020-02-13 09:18:07 +0100

    Setup title of terms and conditions view controller

--- Sources/TinkLinkUI/AddCredential/AddNewCredentials/TermsAndConditionsViewController.swift
+++ Sources/TinkLinkUI/AddCredential/AddNewCredentials/TermsAndConditionsViewController.swift
@@ -1,5 +1,19 @@
 import UIKit
 
 class TermsAndConditionsViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
 
+        title = "Terms & Conditions"
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = Color.background
+    }
 }
