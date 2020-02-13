8b00925e9ee4d08edf6782822ae4796d17b386aa
Kasper Lahti <kasper.lahti@tink.se>
2020-02-13 10:44:37 +0100

    Add web view loading terms and conditions page

--- Sources/TinkLinkUI/AddCredential/AddNewCredentials/TermsAndConditionsViewController.swift
+++ Sources/TinkLinkUI/AddCredential/AddNewCredentials/TermsAndConditionsViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
+import WebKit
 
 final class TermsAndConditionsViewController: UIViewController {
+
+    private var webView: WKWebView { view as! WKWebView }
+
     init() {
         super.init(nibName: nil, bundle: nil)
 
@@ -11,9 +15,18 @@ final class TermsAndConditionsViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func loadView() {
+        view = WKWebView(frame: CGRect(x: 0, y: 0, width: 320, height: 480))
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = Color.background
+
+        let url = URL(string: "https://link.tink.com/terms-and-conditions")!
+        let request = URLRequest(url: url)
+
+        webView.load(request)
     }
 }
