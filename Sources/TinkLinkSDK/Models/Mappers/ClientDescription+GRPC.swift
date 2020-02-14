12e44a712f49a988ef4f18b85f8322f08c028711
Kasper Lahti <kasper.lahti@tink.se>
2020-02-14 12:29:14 +0100

    Swifty property names
    Fix

--- Sources/TinkLinkSDK/Models/Mappers/ClientDescription+GRPC.swift
+++ Sources/TinkLinkSDK/Models/Mappers/ClientDescription+GRPC.swift
@@ -5,9 +5,9 @@ extension ClientDescription {
         self.iconURL = URL(string: grpcDescribeOAuth2ClientResponse.clientIconURL)
         self.name = grpcDescribeOAuth2ClientResponse.clientName
         self.url = URL(string: grpcDescribeOAuth2ClientResponse.clientURL)
-        self.embeddedAllowed = grpcDescribeOAuth2ClientResponse.embeddedAllowed
+        self.isEmbeddedAllowed = grpcDescribeOAuth2ClientResponse.embeddedAllowed
         self.scopes = grpcDescribeOAuth2ClientResponse.scopes.map(ScopeDescription.init(grpcOAuth2ScopeDescription:))
-        self.verified = grpcDescribeOAuth2ClientResponse.verified
-        self.aggregator = grpcDescribeOAuth2ClientResponse.aggregator
+        self.isVerified = grpcDescribeOAuth2ClientResponse.verified
+        self.isAggregator = grpcDescribeOAuth2ClientResponse.aggregator
     }
 }
