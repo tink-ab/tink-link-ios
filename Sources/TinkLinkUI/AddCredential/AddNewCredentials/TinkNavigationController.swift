8eb5e1af2cdee92fc2ee2c98b9479d18bf933f79
Pontus Orraryd <pontus.orraryd@tink.se>
2020-02-14 15:33:42 +0100

    Add navigation controller that obeys appearance

---
+++ Sources/TinkLinkUI/AddCredential/AddNewCredentials/TinkNavigationController.swift
@@ -0,0 +1,61 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pontus Orraryd on 2020-02-14.
+//
+
+import UIKit
+
+class TinkNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Color.background
+        view.tintColor = Color.accent
+        setupNavigationBarAppearance()
+    }
+
+    private func setupNavigationBarAppearance() {
+        navigationBar.tintColor = Color.accent
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithTransparentBackground()
+            appearance.buttonAppearance.normal.titleTextAttributes = [
+                .font: Font.regular(.deci)
+            ]
+            appearance.buttonAppearance.highlighted.titleTextAttributes = [
+                .font: Font.regular(.deci)
+            ]
+
+            appearance.titleTextAttributes = [
+                .font: Font.bold(.hecto),
+                .foregroundColor: Color.label
+            ]
+
+            appearance.backgroundColor = Color.background
+
+            navigationBar.standardAppearance = appearance
+            navigationBar.isTranslucent = false
+        } else {
+
+            // Bar Button Item
+            let barButtonItemAppearance = UIBarButtonItem.appearance(whenContainedInInstancesOf: [TinkLinkViewController.self])
+            barButtonItemAppearance.setTitleTextAttributes([
+                .font: Font.regular(.deci)
+                ], for: .normal)
+            barButtonItemAppearance.setTitleTextAttributes([
+                .font: Font.regular(.deci)
+                ], for: .highlighted)
+
+            navigationBar.titleTextAttributes = [
+                .font: Font.bold(.hecto),
+                .foregroundColor: Color.label
+            ]
+
+            navigationBar.shadowImage = UIImage()
+            navigationBar.barTintColor = Color.background
+            navigationBar.isTranslucent = false
+        }
+    }
+}
