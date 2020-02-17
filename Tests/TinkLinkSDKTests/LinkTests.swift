a658400e6ce5af98ac969b242eecca502e8b694e
Kasper Lahti <kasper.lahti@tink.se>
2020-02-17 11:40:11 +0100

    Test other locales

--- Tests/TinkLinkSDKTests/LinkTests.swift
+++ Tests/TinkLinkSDKTests/LinkTests.swift
@@ -19,4 +19,18 @@ class LinkTests: XCTestCase {
     func testPrivacyPolicyLink() {
         XCTAssertEqual(context.privacyPolicy(), URL(string: "https://link.tink.com/privacy-policy/en"))
     }
+
+    func testPrivacyTermsAndConditionForDifferentLanguages() {
+        XCTAssertEqual(context.termsAndConditions(for: Locale(identifier: "sv_SE")), URL(string: "https://link.tink.com/terms-and-conditions/sv"))
+        XCTAssertEqual(context.termsAndConditions(for: Locale(identifier: "fi_FI")), URL(string: "https://link.tink.com/terms-and-conditions/fi"))
+        XCTAssertEqual(context.termsAndConditions(for: Locale(identifier: "de_DE")), URL(string: "https://link.tink.com/terms-and-conditions/de"))
+        XCTAssertEqual(context.termsAndConditions(for: Locale(identifier: "zh-Hant-HK")), URL(string: "https://link.tink.com/terms-and-conditions/zh"))
+    }
+
+    func testPrivacyPolicyLinkForDifferentLanguages() {
+        XCTAssertEqual(context.privacyPolicy(for: Locale(identifier: "sv_SE")), URL(string: "https://link.tink.com/privacy-policy/sv"))
+        XCTAssertEqual(context.privacyPolicy(for: Locale(identifier: "fi_FI")), URL(string: "https://link.tink.com/privacy-policy/fi"))
+        XCTAssertEqual(context.privacyPolicy(for: Locale(identifier: "de_DE")), URL(string: "https://link.tink.com/privacy-policy/de"))
+        XCTAssertEqual(context.privacyPolicy(for: Locale(identifier: "zh-Hant-HK")), URL(string: "https://link.tink.com/privacy-policy/zh"))
+    }
 }
