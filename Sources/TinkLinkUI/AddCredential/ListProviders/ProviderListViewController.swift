eefb26d1acd6125db3fb89d966bf6b57627f57cc
Mani Sedighi <mani.sedighi@tink.com>
2020-02-19 16:40:59 +0100

    Add separate search view controller

--- Sources/TinkLinkUI/AddCredential/ListProviders/ProviderListViewController.swift
+++ Sources/TinkLinkUI/AddCredential/ListProviders/ProviderListViewController.swift
@@ -7,17 +7,12 @@ final class ProviderListViewController: UITableViewController {
     weak var addCredentialNavigator: AddCredentialFlowNavigating?
 
     private var providerController: ProviderController?
-    private var isSearching: Bool = false
 
-    private let searchController = UISearchController(searchResultsController: nil)
-    private var originalFinancialInstitutionGroupNodes: [ProviderTree.FinancialInstitutionGroupNode] = []
+    private let searchViewController = FinancialInstitutionSearchViewController()
+    
+    private lazy var searchController = UISearchController(searchResultsController: searchViewController)
+    
     private var financialInstitutionGroupNodes: [ProviderTree.FinancialInstitutionGroupNode] = [] {
-        didSet {
-            self.tableView.reloadData()
-        }
-    }
-    private var originalFinancialInstitutionNodes: [ProviderTree.FinancialInstitutionNode] = []
-    private var financialInstitutionNodes: [ProviderTree.FinancialInstitutionNode] = [] {
         didSet {
             self.tableView.reloadData()
         }
@@ -26,10 +21,7 @@ final class ProviderListViewController: UITableViewController {
     init(providerController: ProviderController) {
         self.providerController = providerController
         financialInstitutionGroupNodes = providerController.financialInstitutionGroupNodes
-        originalFinancialInstitutionGroupNodes = providerController.financialInstitutionGroupNodes
-        financialInstitutionNodes = providerController.financialInstitutionNodes
-        originalFinancialInstitutionNodes = providerController.financialInstitutionNodes
-
+        
         super.init(style: .plain)
     }
 
@@ -43,19 +35,19 @@ final class ProviderListViewController: UITableViewController {
 extension ProviderListViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
         NotificationCenter.default.addObserver(self, selector: #selector(showLoadingIndicator), name: .providerControllerWillFetchProviders, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(hideLoadingIndicator), name: .providerControllerDidFetchProviders, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(updateProviders), name: .providerControllerDidUpdateProviders, object: nil)
-
-        searchController.obscuresBackgroundDuringPresentation = false
+        
+        extendedLayoutIncludesOpaqueBars = true
+        
+        searchViewController.addCredentialNavigator = addCredentialNavigator
+        searchController.obscuresBackgroundDuringPresentation = true
         searchController.searchBar.placeholder = "Search for a bank or card"
-        searchController.searchResultsUpdater = self
-
+        searchController.searchResultsUpdater = searchViewController
+ 
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
-
-        definesPresentationContext = true
 
         title = "Choose Bank"
 
@@ -87,9 +79,7 @@ extension ProviderListViewController {
     @objc private func updateProviders() {
         DispatchQueue.main.async {
             self.financialInstitutionGroupNodes = self.providerController?.financialInstitutionGroupNodes ?? []
-            self.originalFinancialInstitutionGroupNodes = self.providerController?.financialInstitutionGroupNodes ?? []
-            self.financialInstitutionNodes = self.providerController?.financialInstitutionNodes ?? []
-            self.originalFinancialInstitutionNodes = self.providerController?.financialInstitutionNodes ?? []
+            self.searchViewController.originalFinancialInstitutionNodes = self.providerController?.financialInstitutionNodes ?? []
         }
     }
 }
@@ -98,31 +88,16 @@ extension ProviderListViewController {
 
 extension ProviderListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if isSearching == true {
-            return financialInstitutionNodes.count
-        } else {
-            return financialInstitutionGroupNodes.count
-        }
-        
+        return financialInstitutionGroupNodes.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        if isSearching {
-            let group = financialInstitutionNodes[indexPath.row]
-            if let imageViewCell = cell as? ProviderCell {
-                imageViewCell.setTitle(text: group.financialInstitution.name)
-                if let url = group.imageURL {
-                    imageViewCell.setImage(url: url)
-                }
-            }
-        } else {
-            let group = financialInstitutionGroupNodes[indexPath.row]
-            if let imageViewCell = cell as? ProviderCell {
-                imageViewCell.setTitle(text: group.displayName)
-                if let url = group.imageURL {
-                    imageViewCell.setImage(url: url)
-                }
+        let group = financialInstitutionGroupNodes[indexPath.row]
+        if let imageViewCell = cell as? ProviderCell {
+            imageViewCell.setTitle(text: group.displayName)
+            if let url = group.imageURL {
+                imageViewCell.setImage(url: url)
             }
         }
         return cell
@@ -139,21 +114,6 @@ extension ProviderListViewController {
             addCredentialNavigator?.showCredentialKindPicker(for: groups, title: nil)
         case .provider(let provider):
             addCredentialNavigator?.showAddCredential(for: provider)
-        }
-    }
-}
-
-// MARK: - UISearchResultsUpdating
-
-extension ProviderListViewController: UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        if let text = searchController.searchBar.text, !text.isEmpty {
-            isSearching = false
-            financialInstitutionNodes = originalFinancialInstitutionNodes.filter { $0.financialInstitution.name.localizedCaseInsensitiveContains(text) }
-        } else {
-            financialInstitutionGroupNodes = originalFinancialInstitutionGroupNodes
-            isSearching = false
-            financialInstitutionNodes = originalFinancialInstitutionNodes
         }
     }
 }
