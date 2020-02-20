2663577ba453aad6963d12a1a898915c0f25cad1
Kasper Lahti <kasper.lahti@tink.se>
2020-02-20 14:38:32 +0100

    Change alert title fallback

--- Sources/TinkLinkUI/AddCredential/ListProviders/TinkLinkViewController.swift
+++ Sources/TinkLinkUI/AddCredential/ListProviders/TinkLinkViewController.swift
@@ -79,7 +79,7 @@ public class TinkLinkViewController: UINavigationController {
         let localizedError = error as? LocalizedError
 
         let alertController = UIAlertController(
-            title: localizedError?.errorDescription ?? "Could not create a temporary user",
+            title: localizedError?.errorDescription ?? "The service is unavailable at the moment.",
             message: localizedError?.failureReason ?? error.localizedDescription,
             preferredStyle: .alert
         )
