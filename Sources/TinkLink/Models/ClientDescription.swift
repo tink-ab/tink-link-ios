03f85a0469d865b6a356f18f538c843aeefb9ea0
Kasper Lahti <kasper.lahti@tink.se>
2020-02-26 16:26:36 +0100

    Document client description

--- Sources/TinkLink/Models/ClientDescription.swift
+++ Sources/TinkLink/Models/ClientDescription.swift
@@ -1,11 +1,14 @@
 import Foundation
 
+/// Description of a client using TinkLink.
 public struct ClientDescription {
     let iconURL: URL?
+    /// The name of the client.
     public let name: String
     let url: URL?
     let isEmbeddedAllowed: Bool
     let scopes: [ScopeDescription]
     let isVerified: Bool
+    /// Whether the client is the aggregator.
     public let isAggregator: Bool
 }
