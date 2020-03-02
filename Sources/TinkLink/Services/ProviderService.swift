5a3a663b8116ce5e91917cd67fc7f398ee3481aa
Menghao Zhang <menghao.zhang@tink.se>
2020-03-02 10:06:09 +0100

    Remove the mapping on service

--- Sources/TinkLink/Services/ProviderService.swift
+++ Sources/TinkLink/Services/ProviderService.swift
@@ -49,9 +49,7 @@ final class ProviderService: TokenConfigurableService {
         request.capability = .unknown
         request.includeTestType = includeTestProviders
 
-        return CallHandler(for: request, method: service.listProviders, queue: queue, responseMap: { $0.providers.map { Provider(grpcProvider: $0) }.filter { !$0.capabilities.isDisjoint(with: capabilities) } }, completion: { result in
-            completion(result.mapError({ ServiceError($0) ?? $0 }))
-        })
+        return CallHandler(for: request, method: service.listProviders, queue: queue, responseMap: { $0.providers.map { Provider(grpcProvider: $0) }.filter { !$0.capabilities.isDisjoint(with: capabilities) } }, completion: completion)
     }
 
     /// Lists all markets where there are providers available.
