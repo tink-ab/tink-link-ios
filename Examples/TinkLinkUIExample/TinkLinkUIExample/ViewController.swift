a6f4bf64d2019ce68aa3efcd8fb3cca775ec1c03
Menghao Zhang <menghao.zhang@tink.se>
2020-03-04 15:25:05 +0100

    Handle the authorization code with in example app

--- Examples/TinkLinkUIExample/TinkLinkUIExample/ViewController.swift
+++ Examples/TinkLinkUIExample/TinkLinkUIExample/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
             Tink.Scope.Categories.read,
             Tink.Scope.Accounts.read
         ])
-        present(TinkLinkViewController(market: "SE", scope: scope), animated: true)
+        let tinkLinkViewController = TinkLinkViewController(market: "SE", scope: scope) { _ in }
+        present(tinkLinkViewController, animated: true)
     }
 }
