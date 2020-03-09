3bb7ca568235bef3cc71e1fdfec06277b4bac5a8
Menghao Zhang <menghao.zhang@tink.se>
2020-03-09 16:10:49 +0100

    Removed the support for the macOS

--- Package.swift
+++ Package.swift
@@ -5,8 +5,7 @@ import PackageDescription
 let package = Package(
     name: "TinkLink",
     platforms: [
-        .iOS(.v11),
-        .macOS(.v10_13)
+        .iOS(.v11)
     ],
     products: [
         .library(
