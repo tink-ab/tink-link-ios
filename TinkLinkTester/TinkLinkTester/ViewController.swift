a304440089694a23f01b0c87554f964d1d5f908b
Kasper Lahti <kasper.lahti@tink.se>
2020-03-12 08:46:37 +0100

    Add button to show tink link flow

--- TinkLinkTester/TinkLinkTester/ViewController.swift
+++ TinkLinkTester/TinkLinkTester/ViewController.swift
@@ -1,10 +1,34 @@
 import UIKit
+import TinkLink
 import TinkLinkUI
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+
+        let button = UIButton(type: .system)
+        button.addTarget(self, action: #selector(showTinkLink), for: .touchUpInside)
+        button.setTitle("Start TinkLink", for: .normal)
+        view.addSubview(button)
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    @objc private func showTinkLink() {
+        let scope = Tink.Scope(scopes: [
+            Tink.Scope.Statistics.read,
+            Tink.Scope.Transactions.read,
+            Tink.Scope.Categories.read,
+            Tink.Scope.Accounts.read
+        ])
+        let tinkLinkViewController = TinkLinkViewController(market: "SE", scope: scope, providerKinds: .all) { _ in }
+        present(tinkLinkViewController, animated: true)
     }
 }
 
