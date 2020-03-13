7681989f6187c19a65e3eebc2a798b94dd7c442e
Menghao Zhang <menghao.zhang@tink.se>
2020-03-13 13:59:11 +0100

    Mocked the credential status updates

---
+++ Tests/TinkLinkTests/Model+mock/Credential+mock.swift
@@ -0,0 +1,31 @@
+@testable import TinkLink
+
+extension Credential {
+    // Extension to update the status for test
+    init(credential: Credential, status: Credential.Status) {
+        Credential(id: credential.id, providerID: credential.providerID, kind: credential.kind, status: status, statusPayload: credential.statusPayload, statusUpdated: credential.statusUpdated, updated: credential.updated, fields: credential.fields, supplementalInformationFields: credential.supplementalInformationFields, thirdPartyAppAuthentication: credential.thirdPartyAppAuthentication, sessionExpiryDate: credential.sessionExpiryDate)
+    }
+
+    func nextCredentialStatus() -> Credential.Status {
+        let nextStatus: Credential.Status = {
+            switch status {
+            case .created, .authenticating:
+                switch kind {
+                case .mobileBankID:
+                    return .awaitingMobileBankIDAuthentication
+                case .thirdPartyAuthentication:
+                    return .awaitingThirdPartyAppAuthentication
+                default:
+                    return .updating
+                }
+            case .awaitingMobileBankIDAuthentication, .awaitingThirdPartyAppAuthentication:
+                return .updating
+            case .updating:
+                return .updated
+            default:
+                return .updated
+            }
+        }()
+        return nextStatus
+    }
+}
