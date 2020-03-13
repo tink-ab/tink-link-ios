4deb3df724b0ae25207ce75fc414becb8fd4bae1
Mani Sedighi <mani.sedighi@tink.com>
2020-03-13 14:51:56 +0100

    Remove setupNavigationBarAppearance from TinkNavigationController

--- Sources/TinkLinkUI/Generic/ViewControllers/TinkNavigationController.swift
+++ Sources/TinkLinkUI/Generic/ViewControllers/TinkNavigationController.swift
@@ -1,50 +1,11 @@
 import UIKit
 
-public class TinkNavigationController: UINavigationController {
+class TinkNavigationController: UINavigationController {
 
-    func setupNavigationBarAppearance() {
-        navigationBar.tintColor = Color.accent
-        if #available(iOS 13.0, *) {
-            let appearance = UINavigationBarAppearance()
-            appearance.buttonAppearance.normal.titleTextAttributes = [
-                .font: Font.regular(.deci)
-            ]
-            appearance.buttonAppearance.highlighted.titleTextAttributes = [
-                .font: Font.regular(.deci)
-            ]
-            let chevronLayer = ChevronLayer()
-            let backIndicatorImage = UIImage.image(from: chevronLayer)
-            appearance.setBackIndicatorImage(backIndicatorImage, transitionMaskImage: backIndicatorImage)
-
-            appearance.titleTextAttributes = [
-                .font: Font.bold(.hecto),
-                .foregroundColor: Color.label
-            ]
-            let navigationBarAppearance = UINavigationBar.appearance(whenContainedInInstancesOf: [TinkLinkViewController.self])
-            navigationBarAppearance.standardAppearance = appearance
-        } else {
-
-            // Bar Button Item
-            let barButtonItemAppearance = UIBarButtonItem.appearance(whenContainedInInstancesOf: [TinkLinkViewController.self])
-            barButtonItemAppearance.setTitleTextAttributes([
-                .font: Font.regular(.deci)
-                ], for: .normal)
-            barButtonItemAppearance.setTitleTextAttributes([
-                .font: Font.regular(.deci)
-                ], for: .highlighted)
-
-            // Navigation Bar
-            let navigationBarAppearance = UINavigationBar.appearance(whenContainedInInstancesOf: [TinkLinkViewController.self])
-
-            let chevronLayer = ChevronLayer()
-            let backIndicatorImage = UIImage.image(from: chevronLayer)
-            navigationBarAppearance.backIndicatorImage = backIndicatorImage
-            navigationBarAppearance.backIndicatorTransitionMaskImage = backIndicatorImage
-
-            navigationBarAppearance.titleTextAttributes = [
-                .font: Font.bold(.hecto),
-                .foregroundColor: Color.label
-            ]
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Color.background
+        view.tintColor = Color.accent
+        setupNavigationBarAppearance()
     }
 }
