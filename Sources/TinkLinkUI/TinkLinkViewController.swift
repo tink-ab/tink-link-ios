4c0913b2ea9f3d394fc7feb8033f83b5b8cbc94b
Mani Sedighi <mani.sedighi@tink.com>
2020-03-13 13:03:46 +0100

    Replace setupNavigationBar in TInkLinkViewController with inheritance

--- Sources/TinkLinkUI/TinkLinkViewController.swift
+++ Sources/TinkLinkUI/TinkLinkViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import TinkLink
 
-public class TinkLinkViewController: UINavigationController {
+public class TinkLinkViewController: TinkNavigationController {
     private let tink: Tink
     private let market: Market
     public let scope: Tink.Scope
@@ -77,49 +77,6 @@ public class TinkLinkViewController: UINavigationController {
                     self.showCreateTemporaryUserAlert(for: error)
                 }
             }
-        }
-    }
-
-    private func setupNavigationBarAppearance() {
-        navigationBar.tintColor = Color.accent
-        if #available(iOS 13.0, *) {
-            let appearance = UINavigationBarAppearance()
-            appearance.configureWithTransparentBackground()
-            appearance.buttonAppearance.normal.titleTextAttributes = [
-                .font: Font.regular(.deci)
-            ]
-            appearance.buttonAppearance.highlighted.titleTextAttributes = [
-                .font: Font.regular(.deci)
-            ]
-
-            appearance.titleTextAttributes = [
-                .font: Font.bold(.hecto),
-                .foregroundColor: Color.label
-            ]
-
-            appearance.backgroundColor = Color.background
-
-            navigationBar.standardAppearance = appearance
-            navigationBar.isTranslucent = false
-        } else {
-
-            // Bar Button Item
-            let barButtonItemAppearance = UIBarButtonItem.appearance(whenContainedInInstancesOf: [TinkLinkViewController.self])
-            barButtonItemAppearance.setTitleTextAttributes([
-                .font: Font.regular(.deci)
-                ], for: .normal)
-            barButtonItemAppearance.setTitleTextAttributes([
-                .font: Font.regular(.deci)
-                ], for: .highlighted)
-
-            navigationBar.titleTextAttributes = [
-                .font: Font.bold(.hecto),
-                .foregroundColor: Color.label
-            ]
-
-            navigationBar.shadowImage = UIImage()
-            navigationBar.barTintColor = Color.background
-            navigationBar.isTranslucent = false
         }
     }
 
