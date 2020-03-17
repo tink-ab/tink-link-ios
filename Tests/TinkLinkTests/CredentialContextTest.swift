d30af250de74bd9399f4d63d0fe4cad616a96736
Menghao Zhang <menghao.zhang@tink.se>
2020-03-17 12:37:44 +0100

    test for adding basic credential

---
+++ Tests/TinkLinkTests/CredentialContextTest.swift
@@ -0,0 +1,57 @@
+import XCTest
+@testable import TinkLink
+
+class CredentialContextTest: XCTestCase {
+    var mockedSuccessCredentialService: MockedSuccessCredentialService!
+    var mockedUnauthenticatedErrorCredentialService: MockedUnauthenticatedErrorCredentialService!
+
+    override func setUp() {
+        mockedSuccessCredentialService = MockedSuccessCredentialService()
+        mockedUnauthenticatedErrorCredentialService = MockedUnauthenticatedErrorCredentialService()
+    }
+
+    func testAddingPasswordCredential() {
+        try! Tink.configure(with: .init(clientID: "testID", redirectURI: URL(string: "app://callback")!))
+        let credentialContextUnderTest = CredentialContext(tink: .shared, credentialService: mockedSuccessCredentialService)
+        let firstFetchCredentialsCompletionCalled = expectation(description: "first fetch credentials completion should be called")
+
+        credentialContextUnderTest.fetchCredentials { result in
+            do {
+                let credentialsList = try result.get()
+                firstFetchCredentialsCompletionCalled.fulfill()
+                XCTAssertEqual(credentialsList.count, 0)
+            } catch {
+                XCTFail("Failed to fetch credentials with: \(error)")
+            }
+        }
+
+        let addCredentialsCompletionCalled = expectation(description: "add credentials completion should be called")
+        let statusChangedToCreated = expectation(description: "add credentials status should be changed to created")
+        let statusChangedToupdating = expectation(description: "add credentials status should be changed to updating")
+
+        let completionPredicate = AddCredentialTask.CompletionPredicate(successPredicate: .updated, shouldFailOnThirdPartyAppAuthenticationDownloadRequired: false)
+        let task = credentialContextUnderTest.addCredential(for: Provider.nordeaPassword, form: Form(provider: Provider.nordeaPassword), completionPredicate: completionPredicate, progressHandler: { status in
+            switch status {
+            case .created:
+                statusChangedToCreated.fulfill()
+            case .authenticating, .awaitingSupplementalInformation, .awaitingThirdPartyAppAuthentication:
+                break
+            case .updating:
+                statusChangedToupdating.fulfill()
+            }
+        }) { result in
+            do {
+                _ = try result.get()
+                addCredentialsCompletionCalled.fulfill()
+            } catch {
+                XCTFail("Failed to create credentials with: \(error)")
+            }
+        }
+
+        waitForExpectations(timeout: 10) { error in
+            if let error = error {
+                XCTFail("waitForExpectations timeout with error: \(error)")
+            }
+        }
+    }
+}
