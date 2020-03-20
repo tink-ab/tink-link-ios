0015e59d8d5020b71206fd9b7d7695f6f6f3480f
Menghao Zhang <menghao.zhang@tink.se>
2020-03-20 14:47:49 +0100

    Service mock for supplemental information

--- Tests/TinkLinkTests/Model+mock/Credential+mock.swift
+++ Tests/TinkLinkTests/Model+mock/Credential+mock.swift
@@ -4,7 +4,17 @@ import Foundation
 extension Credentials {
     // Extension to update the status for test
     init(credentials: Credentials, status: Credentials.Status) {
-        self = Credentials(id: credentials.id, providerID: credentials.providerID, kind: credentials.kind, status: status, statusPayload: credentials.statusPayload, statusUpdated: Date(), updated: Date(), fields: credentials.fields, supplementalInformationFields: credentials.supplementalInformationFields, thirdPartyAppAuthentication: credentials.thirdPartyAppAuthentication, sessionExpiryDate: credentials.sessionExpiryDate)
+        var supplementalInformationFields = [Provider.FieldSpecification]()
+        var thirdPartyAppAuthentication: ThirdPartyAppAuthentication?
+        switch status {
+        case .awaitingSupplementalInformation:
+            supplementalInformationFields = [Provider.FieldSpecification(fieldDescription: "Code", hint: "", maxLength: nil, minLength: nil, isMasked: false, isNumeric: false, isImmutable: false, isOptional: false, name: "code", initialValue: "", pattern: "", patternError: "", helpText: "")]
+        case .awaitingMobileBankIDAuthentication, .awaitingThirdPartyAppAuthentication:
+            thirdPartyAppAuthentication = ThirdPartyAppAuthentication(downloadTitle: "Test download title", downloadMessage: "Test download message", upgradeTitle: "Test upgrade title", upgradeMessage: "Test upgrade message", appStoreURL: nil, scheme: "app", deepLinkURL: URL(string: "testApp://callback"))
+        default:
+            break
+        }
+        self = Credentials(id: credentials.id, providerID: credentials.providerID, kind: credentials.kind, status: status, statusPayload: credentials.statusPayload, statusUpdated: Date(), updated: Date(), fields: credentials.fields, supplementalInformationFields: supplementalInformationFields, thirdPartyAppAuthentication: credentials.thirdPartyAppAuthentication, sessionExpiryDate: credentials.sessionExpiryDate)
     }
 
     func nextCredentialsStatus() -> Credentials.Status {
@@ -16,10 +26,14 @@ extension Credentials {
                     return .awaitingMobileBankIDAuthentication
                 case .thirdPartyAuthentication:
                     return .awaitingThirdPartyAppAuthentication
+                case .password where self.providerID == "se-test-multi-supplemental":
+                    return .awaitingSupplementalInformation
                 default:
                     return .updating
                 }
-            case .awaitingMobileBankIDAuthentication, .awaitingThirdPartyAppAuthentication:
+            case .awaitingMobileBankIDAuthentication,
+                 .awaitingThirdPartyAppAuthentication,
+                 .awaitingSupplementalInformation:
                 return .updating
             case .updating:
                 return .updated
