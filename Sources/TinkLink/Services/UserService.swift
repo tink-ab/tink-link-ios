43fb3cd4026e9d311f0aee335111fdc3f4035da3
Pontus Orraryd <pontus.orraryd@tink.se>
2020-03-23 14:07:06 +0100

    Add auth endpoint to rest user service

--- Sources/TinkLink/Services/UserService.swift
+++ Sources/TinkLink/Services/UserService.swift
@@ -22,6 +22,15 @@ final class RESTUserService {
         let request = RESTResourceRequest(path: "/api/v1/user/profile", method: .get, contentType: .json, completion: completion)
         return client.performRequest(request)
     }
+
+    @discardableResult
+    func authenticate(code: AuthorizationCode, completion: @escaping (Result<AuthenticateResponse, Error>) -> Void) -> Cancellable? {
+        var request = RESTResourceRequest(path: "/link/v1/authentication/token", method: .post, contentType: .json, completion: completion)
+        let body = ["code": code.rawValue]
+        request.body = try? JSONEncoder().encode(body)
+
+        return client.performRequest(request)
+    }
 }
 
 final class TinkUserService: UserService, TokenConfigurableService {
