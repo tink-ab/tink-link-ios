6fb3ee7ce47ce1d89f803f42c42a5735906cb8e4
Pontus Orraryd <pontus.orraryd@tink.se>
2020-03-23 15:34:39 +0100

    Changes to Provider service

--- Sources/TinkLink/REST/Services/RESTProviderService.swift
+++ Sources/TinkLink/REST/Services/RESTProviderService.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-final class RESTProviderService {
+final class RESTProviderService: ProviderService {
 
     private let client: RESTClient
 
@@ -8,17 +8,14 @@ final class RESTProviderService {
         self.client = client
     }
 
-    func providers(capabilities: Set<RESTProvider.Capabilities>? = nil, includeTestProviders: Bool = false, completion: @escaping (Result<[RESTProvider], Error>) -> Void) -> Cancellable? {
+    func providers(market: Market?, capabilities: Provider.Capabilities, includeTestProviders: Bool, completion: @escaping (Result<[Provider], Error>) -> Void) -> RetryCancellable? {
 
         let parameters = ["includeTestProviders": includeTestProviders ? "true" : "false"]
 
         let request = RESTResourceRequest<RESTProviders>(path: "/api/v1/providers", method: .get, contentType: .json, parameters: parameters) { result in
 
-            //TODO: This is ugly
-            var result = result.map { $0.providers }
-            if let capabilities = capabilities {
-                result = result.map { $0.filter { !capabilities.intersection($0.capabilities).isEmpty } }
-            }
+            let result = result.map { $0.providers.map(Provider.init).filter { !$0.capabilities.isDisjoint(with: capabilities) } }
+
             completion(result)
         }
 
