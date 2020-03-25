c9ea5b29530dc8a49b614cf81386c8c423871d83
Kasper Lahti <kasper.lahti@tink.se>
2020-03-25 08:35:27 +0100

    Fix indentation

--- Sources/TinkLinkUI/Generic/Extensions/AddCredentialTaskError+Localized.swift
+++ Sources/TinkLinkUI/Generic/Extensions/AddCredentialTaskError+Localized.swift
@@ -7,11 +7,11 @@ extension AddCredentialsTask.Error: LocalizedError {
         case .permanentFailure:
             return NSLocalizedString("AddCredentialsTask.Error.PermanentFailure", tableName: "TinkLink", value: "Permanent error", comment: "Title for error shown when a permanent failure occured while adding credentials.")
         case .temporaryFailure:
-                return NSLocalizedString("AddCredentialsTask.Error.TemporaryFailure", tableName: "TinkLink", value: "Temporary error", comment: "Title for error shown when a temporary failure occured while adding credentials.")
+            return NSLocalizedString("AddCredentialsTask.Error.TemporaryFailure", tableName: "TinkLink", value: "Temporary error", comment: "Title for error shown when a temporary failure occured while adding credentials.")
         case .authenticationFailed:
-                return NSLocalizedString("AddCredentialsTask.Error.AuthenticationFailed", tableName: "TinkLink", value: "Authentication failed", comment: "Title for error shown when authentication failed while adding credentials.")
+            return NSLocalizedString("AddCredentialsTask.Error.AuthenticationFailed", tableName: "TinkLink", value: "Authentication failed", comment: "Title for error shown when authentication failed while adding credentials.")
         case .credentialsAlreadyExists:
-                return NSLocalizedString("AddCredentialsTask.Error.CredentialsAlreadyExists", tableName: "TinkLink", value: "Error", comment: "Title for error shown when credentials already exists.")
+            return NSLocalizedString("AddCredentialsTask.Error.CredentialsAlreadyExists", tableName: "TinkLink", value: "Error", comment: "Title for error shown when credentials already exists.")
         }
     }
 
