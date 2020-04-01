85895949c9712102d3839178f016e116fbf3e15e
Kasper Lahti <kasper.lahti@tink.se>
2020-04-01 09:45:18 +0200

    Remove mention of redirect after a few seconds

--- Sources/TinkLinkUI/Flows/AddCredential/ViewControllers/CredentialSuccessfullyAddedViewController.swift
+++ Sources/TinkLinkUI/Flows/AddCredential/ViewControllers/CredentialSuccessfullyAddedViewController.swift
@@ -23,7 +23,7 @@ class CredentialSuccessfullyAddedViewController: UIViewController {
     
     //TODO: Use real strings
     private let titleText = NSLocalizedString("AddCredentials.Success.Title", tableName: "TinkLinkUI", bundle: .tinkLinkUI, value: "Connection successful", comment: "Title for screen shown when credentials were added successfully.")
-    private let subtitleText = NSLocalizedString("AddCredentials.Success.Subtitle", tableName: "TinkLinkUI", bundle: .tinkLinkUI, value: "Your account has successfully connected to %@. You'll be redirected back in a few seconds...", comment: "Subtitle for screen shown when credentials were added successfully.")
+    private let subtitleText = NSLocalizedString("AddCredentials.Success.Subtitle", tableName: "TinkLinkUI", bundle: .tinkLinkUI, value: "Your account has successfully connected to %@.", comment: "Subtitle for screen shown when credentials were added successfully.")
     
     override func viewDidLoad() {
         super.viewDidLoad()
