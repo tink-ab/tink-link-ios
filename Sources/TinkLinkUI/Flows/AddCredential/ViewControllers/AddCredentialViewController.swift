c6708e0cc6fea6517dd2fd5ac03298fcf5612c6d
Kasper Lahti <kasper.lahti@tink.se>
2020-04-01 11:57:22 +0200

    Include button height when adjusting content inset

--- Sources/TinkLinkUI/Flows/AddCredential/ViewControllers/AddCredentialViewController.swift
+++ Sources/TinkLinkUI/Flows/AddCredential/ViewControllers/AddCredentialViewController.swift
@@ -140,7 +140,7 @@ extension AddCredentialViewController {
         tableView.tableHeaderView = headerView
         tableView.tableHeaderView?.frame = frame
 
-        tableView.contentInset.bottom = buttonBottomConstraint.constant + addCredentialFooterView.frame.height
+        tableView.contentInset.bottom = button.frame.height + buttonBottomConstraint.constant + addCredentialFooterView.frame.height
     }
 
     override func viewLayoutMarginsDidChange() {
