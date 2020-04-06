309a58f9c9c5df7ecf4bdff2f1b1ef945a5ad241
Pontus Orraryd <pontus.orraryd@tink.se>
2020-04-06 11:19:27 +0200

    Use default provider kinds for example project

--- Examples/TinkLinkUIExample/TinkLinkUIExample/ViewController.swift
+++ Examples/TinkLinkUIExample/TinkLinkUIExample/ViewController.swift
@@ -46,7 +46,7 @@ class ViewController: UIViewController {
             .categories(.read),
             .accounts(.read)
         ]
-        let tinkLinkViewController = TinkLinkViewController(market: "SE", scopes: scopes, providerKinds: .all) { _ in }
+        let tinkLinkViewController = TinkLinkViewController(market: "SE", scopes: scopes) { _ in }
         present(tinkLinkViewController, animated: true)
     }
 }
