6424aabf46f7772fee66419ea480d56744d28b66
Kasper Lahti <kasper.lahti@tink.se>
2020-04-15 16:24:53 +0200

    Remove refresh alert view and refresh button

--- Examples/PermanentUserProviderSelection/PermanentUserProviderSelection/CredentialsView.swift
+++ Examples/PermanentUserProviderSelection/PermanentUserProviderSelection/CredentialsView.swift
@@ -21,37 +21,6 @@ struct CredentialsView: View {
                 CredentialsList(credentialsController: credentialsController, providerController: providerController)
                     .disabled(isPresentingRefreshAlert)
                     .navigationBarTitle("Credentials")
-                    .navigationBarItems(
-                        trailing: Button(action: {
-                            self.isPresentingRefreshAlert = true
-                        }) {
-                            Image(systemName: "arrow.clockwise")
-                                .padding(.all, 10)
-                        }
-                )
-                .alertView(isPresented: $isPresentingRefreshAlert) {
-                    AlertView(
-                        title: isRefreshing ? "Updating…" : "Update banks & services",
-                        content: {
-                            RefreshCredentialsList(credentials: self.credentialsController.credentials, updatedCredentials: self.credentialsController.updatedCredentials, providerController: self.providerController, selectedCredentials: self.$selectedCredentials)
-                                .padding(.horizontal, 16)
-                                .padding(.bottom, 20)
-                                .disabled(isRefreshing)
-                        },
-                        dismissButton: .cancel(Text("Cancel"), action: {
-                            self.isRefreshing = false
-                            self.isPresentingRefreshAlert = false
-                            self.credentialsController.cancelRefresh()
-                        }),
-                        primaryButton: .default(Text("Update"), enabled: selectedCredentials != nil, action: {
-                            self.isRefreshing = true
-                            self.credentialsController.performRefresh(credentials: self.selectedCredentials!) { _ in
-                                self.isRefreshing = false
-                                self.isPresentingRefreshAlert = false
-                            }
-                        })
-                    )
-                }
                 .sheet(item: Binding(get: { self.credentialsController.supplementInformationTask }, set: { self.credentialsController.supplementInformationTask = $0 })) {
                     SupplementControllerRepresentableView(supplementInformationTask: $0) { _ in }
                 }
