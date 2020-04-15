35f4ee48c81e0a5771892595e6c44398e3f1f1fb
Kasper Lahti <kasper.lahti@tink.se>
2020-04-15 16:28:22 +0200

    Add detail view

---
+++ Examples/PermanentUserProviderSelection/PermanentUserProviderSelection/CredentialList/CredentialsDetailView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import TinkLink
+
+struct CredentialsDetailView: View {
+    let credentials: Credentials
+
+    var body: some View {
+        Form {
+            Text(credentials.statusPayload)
+            Button(action: refresh) {
+                Text("Refresh")
+            }
+        }
+    }
+
+    private func refresh() {
+
+    }
+}
