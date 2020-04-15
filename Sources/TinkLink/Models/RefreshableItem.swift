0d1e225a8bbf02982981fbc99d58b26a1f70d6a1
Pontus Orraryd <pontus.orraryd@tink.se>
2020-04-15 16:38:34 +0200

    Add helpers to RefreshableItem

--- Sources/TinkLink/Models/RefreshableItem.swift
+++ Sources/TinkLink/Models/RefreshableItem.swift
@@ -14,28 +14,41 @@ public enum RefreshableItem: String, CaseIterable {
     case identityData = "IDENTITY_DATA"
 }
 
+// MARK: Helper properties
+
+extension RefreshableItem {
+    static var accountItems: Set<RefreshableItem> { [
+        .checkingAccounts,
+        .savingAccounts,
+        .creditCardAccounts,
+        .loanAccounts,
+        .investmentAccounts
+    ]}
+
+    static var transactionItems: Set<RefreshableItem> { [
+        .checkingTransactions,
+        .savingTransactions,
+        .creditCardTransactions,
+        .loanTransactions,
+        .investmentTransactions
+    ]}
+}
+
+// MARK: Scope mapping
+
 extension RefreshableItem {
     static func makeFromScopes(_ scopes: [Scope], provider: Provider) -> Set<RefreshableItem> {
 
         let providerCapabilities = provider.capabilities
 
-        var requestedRefreshableItems: Set<RefreshableItem> = [
-            .checkingAccounts,
-            .savingAccounts,
-            .creditCardAccounts,
-            .loanAccounts,
-            .investmentAccounts,
+        var requestedRefreshableItems: Set<RefreshableItem> =
+            RefreshableItem.accountItems.union([
             .eInvoices,
-            .transferDestinations]
+            .transferDestinations])
 
         // Based on: https://github.com/tink-ab/tink-backend/blob/39c97c74a0eba4d039b5347de3781df378c3692f/src/main-system-features/aggregation_controller_v1/src/main/java/se/tink/libraries/aggregation_controller_v1/enums/RefreshableItem.java#L36
         if scopes.scopeDescription.contains("transactions:read") {
-            requestedRefreshableItems.formUnion([
-                .checkingTransactions,
-                .savingTransactions,
-                .creditCardTransactions,
-                .loanTransactions,
-                .investmentTransactions])
+            requestedRefreshableItems.formUnion(RefreshableItem.transactionItems)
         }
 
         if scopes.scopeDescription.contains("identity:read") {
@@ -43,7 +56,7 @@ extension RefreshableItem {
         }
 
         // This makes sure that at least one of the provider capability that maps to
-        // that refreshable item is supported by the provider, otherwise an error would be 
+        // that refreshable item is supported by the provider, otherwise an error would be
         // raised because of provider capability validation on the backend
         return requestedRefreshableItems.filter { (item) -> Bool in
             let capabilities = makeProviderCapabilities(refreshableItem: item)
