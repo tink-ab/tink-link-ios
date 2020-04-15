f0ccff1a461167a76f92d78991dccfe9bbd4296d
Kasper Lahti <kasper.lahti@tink.se>
2020-04-15 16:42:58 +0200

    Remove unused observed objects

--- Examples/PermanentUserProviderSelection/PermanentUserProviderSelection/ContentView.swift
+++ Examples/PermanentUserProviderSelection/PermanentUserProviderSelection/ContentView.swift
@@ -2,10 +2,6 @@ import SwiftUI
 import TinkLink
 
 struct ContentView: View {
-
-    @ObservedObject var credentialsController = CredentialsController()
-    @ObservedObject var providerController = ProviderController()
-
     private let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm MMM dd, yyyy"
@@ -21,6 +17,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(credentialsController: CredentialsController(), providerController: ProviderController())
+        ContentView()
     }
 }
