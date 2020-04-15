4dc7f1c66288c19ab01483950c5da1e1551f9efb
Kasper Lahti <kasper.lahti@tink.se>
2020-04-15 09:12:12 +0200

    Remove unused date formatter

--- Examples/PermanentUserProviderSelection/PermanentUserProviderSelection/ContentView.swift
+++ Examples/PermanentUserProviderSelection/PermanentUserProviderSelection/ContentView.swift
@@ -2,12 +2,6 @@ import SwiftUI
 import TinkLink
 
 struct ContentView: View {
-    private let dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm MMM dd, yyyy"
-        return formatter
-    }()
-
     var body: some View {
         NavigationView {
             CredentialsView()
