185e9631c6e500376d958d987b8d75f4eca98eac
Kasper Lahti <kasper.lahti@tink.se>
2020-04-16 14:33:06 +0200

    Post awaitAuthenticationOnAnotherDevice status if there is no qr code for bank id credentials

--- Sources/TinkLink/Tasks/ThirdPartyAppAuthenticationTask.swift
+++ Sources/TinkLink/Tasks/ThirdPartyAppAuthenticationTask.swift
@@ -235,11 +235,18 @@ public class ThirdPartyAppAuthenticationTask: Identifiable {
             }
         }
         #elseif os(macOS)
-        qr { [weak self] result in
+        qr { [weak self, credentialsKind = credentials.kind] result in
             do {
                 let qrImage = try result.get()
                 statusHandler(.qrImage(qrImage))
                 self?.completionHandler(.success)
+            } catch Error.doesNotSupportAuthenticatingOnAnotherDevice {
+                if credentialsKind == .mobileBankID {
+                    statusHandler(.awaitAuthenticationOnAnotherDevice)
+                    self?.completionHandler(.success)
+                } else {
+                    self?.completionHandler(.failure(Error.doesNotSupportAuthenticatingOnAnotherDevice))
+                }
             } catch {
                 self?.completionHandler(.failure(error))
             }
