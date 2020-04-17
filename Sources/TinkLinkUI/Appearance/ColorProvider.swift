2e97039795014f25c4998a11f856eecc8a9da93b
Mani Sedighi <mani.sedighi@tink.com>
2020-04-17 10:12:56 +0200

    Add ColorProvider class

---
+++ Sources/TinkLinkUI/Appearance/ColorProvider.swift
@@ -0,0 +1,86 @@
+import UIKit
+
+/// A type that can provide custom colors for Tink views.
+public class ColorProvider: ColorProviding {
+
+    /// Color for the main background of the interface.
+    public var background: UIColor = .white
+    /// Color for content layered on top of the main background.
+    public var secondaryBackground: UIColor = .white
+    /// Color for the main background of grouped interface components.
+    public var groupedBackground: UIColor = UIColor(red: 251.0 / 255.0, green: 251.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
+    /// Color for content layered on top of the main background of grouped interface components.
+    public var secondaryGroupedBackground: UIColor = .white
+    /// Primary text color.
+    public var label: UIColor = UIColor(red: 0.149, green: 0.149, blue: 0.149, alpha: 1.0)
+    /// Secondary text color.
+    public var secondaryLabel: UIColor = UIColor(red: 0.502, green: 0.502, blue: 0.502, alpha: 1.0)
+    /// Color for separators.
+    public var separator: UIColor = UIColor(red: 0.933, green: 0.933, blue: 0.933, alpha: 1.0)
+    /// Colors for buttons, indicators and other similar elements.
+    public var accent: UIColor = UIColor(red: 0.259, green: 0.467, blue: 0.514, alpha: 1.0)
+
+    /// Color to represent expenses.
+    public var expenses: UIColor = UIColor(red: 66.0 / 255.0, green: 119.0 / 255.0, blue: 131.0 / 255.0, alpha: 1.0)
+    /// Color to represent incomes.
+    public var income: UIColor = UIColor(red: 0.212, green: 0.706, blue: 0.447, alpha: 1.0)
+    /// Color to represent transfers.
+    public var transfers: UIColor = UIColor(red: 0.282, green: 0.282, blue: 0.282, alpha: 1.0)
+    /// Color representing uncategorized transactions.
+    public var uncategorized: UIColor = UIColor(red: 0.996, green: 0.682, blue: 0.133, alpha: 1.0)
+    /// Color representing a warning.
+    public var warning: UIColor = UIColor(red: 0.996, green: 0.682, blue: 0.133, alpha: 1.0)
+    /// Color representing a critical error or warning.
+    public var critical: UIColor = UIColor(red: 0.996, green: 0.682, blue: 0.133, alpha: 1.0)
+
+    public init() {}
+
+    /// Initializes a color provider with the specified styling.
+    ///
+    /// - Parameters:
+    ///   - background: Color for the main background of the interface.
+    ///   - secondaryBackground: Color for content layered on top of the main background.
+    ///   - groupedBackground: Color for the main background of grouped interface components.
+    ///   - secondaryGroupedBackground: Color for content layered on top of the main background of grouped interface components.
+    ///   - label: Primary text color.
+    ///   - secondaryLabel: Secondary text color.
+    ///   - separator: Color for separators.
+    ///   - accent: Colors for buttons, indicators and other similar elements.
+    ///   - expenses: Color to represent expenses.
+    ///   - income: Color to represent incomes.
+    ///   - transfers: Color to represent transfers.
+    ///   - uncategorized:  Color representing uncategorized transactions.
+    ///   - warning:  Color representing a warning.
+    ///   - critical: Color representing a critical error or warning
+    public init(
+        background: UIColor,
+        secondaryBackground: UIColor,
+        groupedBackground: UIColor,
+        secondaryGroupedBackground: UIColor,
+        label: UIColor,
+        secondaryLabel: UIColor,
+        separator: UIColor,
+        accent: UIColor,
+        expenses: UIColor,
+        income: UIColor,
+        transfers: UIColor,
+        uncategorized: UIColor,
+        warning: UIColor,
+        critical: UIColor
+    ) {
+        self.background = background
+        self.secondaryBackground = secondaryBackground
+        self.groupedBackground = groupedBackground
+        self.secondaryGroupedBackground = secondaryGroupedBackground
+        self.label = label
+        self.secondaryLabel = secondaryLabel
+        self.separator = separator
+        self.accent = accent
+        self.expenses = expenses
+        self.income = income
+        self.transfers = transfers
+        self.uncategorized = uncategorized
+        self.warning = warning
+        self.critical = critical
+    }
+}
