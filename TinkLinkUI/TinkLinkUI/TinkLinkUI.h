71bbdaefbf00838b1faa43e360b576eb89c60636
Kasper Lahti <kasper.lahti@tink.se>
2020-04-17 10:52:05 +0200

    Add TinkLinkUI target

---
+++ TinkLinkUI/TinkLinkUI/TinkLinkUI.h
@@ -0,0 +1,18 @@
+//
+//  TinkLinkUI.h
+//  TinkLinkUI
+//
+//  Created by Kasper Lahti on 2020-04-17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TinkLinkUI.
+FOUNDATION_EXPORT double TinkLinkUIVersionNumber;
+
+//! Project version string for TinkLinkUI.
+FOUNDATION_EXPORT const unsigned char TinkLinkUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinkLinkUI/PublicHeader.h>
+
+
