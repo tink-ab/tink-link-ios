8243a63233a95bc051d43b965caf0816ef95e211
Menghao Zhang <menghao.zhang@tink.se>
2020-04-17 14:39:07 +0200

    Docs for error

--- Sources/TinkLinkUI/TinkLinkError.swift
+++ Sources/TinkLinkUI/TinkLinkError.swift
@@ -1,8 +1,12 @@
 import Foundation
 
+/// Error that returned by the TInkLinkUI when failed to aggreagate the credential
 public enum TinkLinkError: Error {
+    /// User cancelled the flow.
     case userCancelled
+    /// Unable to fetch any back.
     case unableToFetchBanks
+    /// Lost internet connection.
     case missingInternetConnection
 
     init?(error: Error) {
