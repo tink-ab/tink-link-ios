3d8f0105a0dfa85cdaf1ad7b2b63a75304b73592
Menghao Zhang <menghao.zhang@tink.se>
2020-04-20 16:23:44 +0200

    Wording

--- Sources/TinkLinkUI/Appearance/AppearanceProviding.swift
+++ Sources/TinkLinkUI/Appearance/AppearanceProviding.swift
@@ -13,7 +13,7 @@ public struct AppearanceProvider: AppearanceProviding {
     /// Font provier
     public let fonts: FontProviding
 
-    /// Create customized Appearence with specific provider, if no provider is provide, the default value will be used.
+    /// Create customized Appearence with specific provider, if no value is passed, the default provider will be used.
     public init(
         colors: ColorProvider? = nil,
         fonts: FontProvider? = nil
