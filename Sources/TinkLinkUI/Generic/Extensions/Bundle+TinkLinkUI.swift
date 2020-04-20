667ef6b41ae678b22462ae6fed923366346ba7cc
Mani Sedighi <mani.sedighi@tink.com>
2020-04-20 16:08:13 +0200

    Replace base64 icons with png

--- Sources/TinkLinkUI/Generic/Extensions/Bundle+TinkLinkUI.swift
+++ Sources/TinkLinkUI/Generic/Extensions/Bundle+TinkLinkUI.swift
@@ -4,4 +4,5 @@ private class BundleLoadingClass { }
 
 extension Bundle {
     static let tinkLinkUI = Bundle(url: Bundle(for: BundleLoadingClass.self).url(forResource: "Translations", withExtension: "bundle")!)!
+    static let assetBundle = Bundle(url: Bundle(for: BundleLoadingClass.self).url(forResource: "Assets", withExtension: "bundle")!)! 
 }
