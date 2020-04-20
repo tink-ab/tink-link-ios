9f490e2418f4206d3c47934c8905fb4ef75ccefb
Menghao Zhang <menghao.zhang@tink.se>
2020-04-20 16:10:01 +0200

    Docs on the appearence provider

--- Sources/TinkLinkUI/Appearance/AppearanceProviding.swift
+++ Sources/TinkLinkUI/Appearance/AppearanceProviding.swift
@@ -8,9 +8,12 @@ public protocol AppearanceProviding {
 
 /// A appearance provider that can provide colors and fonts for Tink views.
 public struct AppearanceProvider: AppearanceProviding {
+    /// Color provier
     public let colors: ColorProviding
+    /// Font provier
     public let fonts: FontProviding
 
+    /// Create customized Appearence with specific provider, if no provider is provide, the default value will be used.
     public init(
         colors: ColorProvider? = nil,
         fonts: FontProvider? = nil
