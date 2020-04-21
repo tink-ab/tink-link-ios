0411f0a9bf8c9373b48188f2173e79e03c78c6c0
Kasper Lahti <kasper.lahti@tink.se>
2020-04-21 11:55:40 +0200

    Remove semibold from concrete font provider

--- Sources/TinkLinkUI/Appearance/FontProvider.swift
+++ Sources/TinkLinkUI/Appearance/FontProvider.swift
@@ -6,8 +6,6 @@ public class FontProvider: FontProviding {
     public var lightFont: Font = .systemDefault
     /// Regular font.
     public var regularFont: Font = .systemDefault
-    /// Semibold font.
-    public var semiBoldFont: Font = .systemDefault
     /// Bold font.
     public var boldFont: Font = .systemDefault
 
@@ -18,17 +16,14 @@ public class FontProvider: FontProviding {
     /// - Parameters:
     ///   - lightFont: The light font to use.
     ///   - regularFont: The regular font to use.
-    ///   - semiBoldFont: The semibold font to use.
     ///   - boldFont: The bold font to use.
     public init(
         lightFont: Font,
         regularFont: Font,
-        semiBoldFont: Font,
         boldFont: Font
     ) {
         self.lightFont = lightFont
         self.regularFont = regularFont
-        self.semiBoldFont = semiBoldFont
         self.boldFont = boldFont
     }
 
