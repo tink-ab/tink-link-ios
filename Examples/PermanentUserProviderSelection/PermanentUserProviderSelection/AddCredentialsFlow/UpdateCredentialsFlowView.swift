075c4c2bfd91fb11743743224748b6b2bd3a23f7
Menghao Zhang <menghao.zhang@tink.se>
2020-05-05 13:53:57 +0200

    removed the header

--- Examples/PermanentUserProviderSelection/PermanentUserProviderSelection/AddCredentialsFlow/UpdateCredentialsFlowView.swift
+++ Examples/PermanentUserProviderSelection/PermanentUserProviderSelection/AddCredentialsFlow/UpdateCredentialsFlowView.swift
@@ -1,11 +1,3 @@
-//
-//  UpdateCredentialsFlowView.swift
-//  PermanentUserProviderSelection
-//
-//  Created by Menghao Zhang on 2020-04-30.
-//  Copyright © 2020 Tink. All rights reserved.
-//
-
 import SwiftUI
 import TinkLink
 
