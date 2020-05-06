c63b6039c29f98e8b89be553089c284c5168a3b0
Menghao Zhang <menghao.zhang@tink.se>
2020-05-06 17:30:49 +0200

    add service with mobile data

--- Sources/TinkLink/REST/Services/Payment/RESTTransferService.swift
+++ Sources/TinkLink/REST/Services/Payment/RESTTransferService.swift
@@ -8,23 +8,34 @@ final class RESTTransferService {
     }
 
     // TOOD: use mobile transfer model when creating the transfer
-    func transfer(transfer: RESTTransferRequest, completion: @escaping (Result<RESTTransferResponse, Error>) -> Void) -> RetryCancellable? {
-        let data = try? JSONEncoder().encode(transfer)
+    func transfer(transfer: Transfer, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable? {
+        let body = RESTTransferRequest(
+            amount: transfer.amount.doubleValue,
+            credentialsId: transfer.credentialsID.value,
+            currency: transfer.currency.value,
+            sourceMessage: transfer.sourceMessage,
+            destinationMessage: transfer.destinationMessage,
+            id: transfer.id.value,
+            dueDate: transfer.dueDate,
+            messageType: transfer.messageType.rawValue,
+            sourceUri: transfer.sourceUri.value,
+            destinationUri: transfer.destinationUri.value)
+        let data = try? JSONEncoder().encode(body)
 
-        let request = RESTResourceRequest<RESTTransferResponse>(path: "/api/v1/transfer", method: .post, body: data, contentType: .json) { result in
-            // TODO: map to the mobile transfer response model
-            completion(result)
+        let request = RESTResourceRequest<RESTSignableOperation>(path: "/api/v1/transfer", method: .post, body: data, contentType: .json) { result in
+            let mappedResult = result.map{ SignableOperation($0) }
+            completion(mappedResult)
         }
 
         return client.performRequest(request)
     }
 
     // TOOD: use mobile transfer model ID when getting the transfer status
-    func transferStatus(transferID: String, completion: @escaping (Result<RESTTransferResponse, Error>) -> Void) -> RetryCancellable? {
+    func transferStatus(transferID: String, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable? {
 
-        let request = RESTResourceRequest<RESTTransferResponse>(path: "/api/v1/transfer/\(transferID)/status", method: .get, contentType: .json) { result in
-            // TODO: map to the mobile transfer response model
-            completion(result)
+        let request = RESTResourceRequest<RESTSignableOperation>(path: "/api/v1/transfer/\(transferID)/status", method: .get, contentType: .json) { result in
+            let mappedResult = result.map{ SignableOperation($0) }
+            completion(mappedResult)
         }
 
         return client.performRequest(request)
