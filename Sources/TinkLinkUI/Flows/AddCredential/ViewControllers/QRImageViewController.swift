87815e15d40a354d3f5f5a64dccce15115265bef
Kasper Lahti <kasper.lahti@tink.se>
2020-05-07 08:25:11 +0200

    Update font used in qr image view to match design

--- Sources/TinkLinkUI/Flows/AddCredential/ViewControllers/QRImageViewController.swift
+++ Sources/TinkLinkUI/Flows/AddCredential/ViewControllers/QRImageViewController.swift
@@ -50,7 +50,7 @@ final class QRImageViewController: UIViewController {
         subtitleLabel.text = "Open the BankID app"
         subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
 
-        descriptionLabel.font = Font.body
+        descriptionLabel.font = Font.footnote
         descriptionLabel.textColor = Color.label
         descriptionLabel.numberOfLines = 0
         descriptionLabel.textAlignment = .center
