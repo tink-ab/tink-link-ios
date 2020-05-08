e0ca9e1022776f3466ca5f1e39fd41d14407cd7f
Kasper Lahti <kasper.lahti@tink.se>
2020-05-08 15:56:31 +0200

    Use localized error conformance to present better error messages

--- Examples/PermanentUserExample-SwiftUI/PermanentUserExample/AddCredentialsFlow/AddCredentials/AddNewCredentials/UpdateCredentialsViewController.swift
+++ Examples/PermanentUserExample-SwiftUI/PermanentUserExample/AddCredentialsFlow/AddCredentials/AddNewCredentials/UpdateCredentialsViewController.swift
@@ -276,7 +276,12 @@ extension UpdateCredentialsViewController {
     }
 
     private func showAlert(for error: Error) {
-        let alertController = UIAlertController(title: "Error", message: String(describing: error), preferredStyle: .alert)
+        let localizedError = error as? LocalizedError
+        let alertController = UIAlertController(
+            title: localizedError?.errorDescription ?? "Error",
+            message: localizedError?.failureReason ?? error.localizedDescription,
+            preferredStyle: .alert
+        )
 
         let okAction = UIAlertAction(title: "OK", style: .default)
         alertController.addAction(okAction)
