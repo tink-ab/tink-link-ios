318b13e47f4a16828501c2675bac6b9baec80004
Kasper Lahti <kasper.lahti@tink.se>
2020-05-11 14:23:38 +0200

    Add transfer method

--- Examples/PermanentUserExample/PermanentUserExample/Transfer/TransferViewController.swift
+++ Examples/PermanentUserExample/PermanentUserExample/Transfer/TransferViewController.swift
@@ -4,11 +4,27 @@ import TinkLink
 class TransferViewController: UITableViewController {
     private let transferContext = TransferContext()
 
+    var credentials: Credentials!
+
     init() {
         super.init(style: .insetGrouped)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc private func transfer(_ sender: Any) {
+        _ = transferContext.initiateTransfer(
+            amount: ExactNumber(value: 1),
+            currencyCode: "EUR",
+            credentials: credentials,
+            sourceURI: Transfer.TransferEntityURI("SOURCE_URI"),
+            destinationURI: Transfer.TransferEntityURI("DESTINATION_URI"),
+            message: "MESSAGE",
+            completion: { result in
+                dump(result)
+            }
+        )
     }
 }
