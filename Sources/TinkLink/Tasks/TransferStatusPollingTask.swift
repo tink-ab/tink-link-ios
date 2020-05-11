e994544a800b4d594010c955413e084a55a9e8b1
Menghao Zhang <menghao.zhang@tink.se>
2020-05-11 17:38:29 +0200

    Add transfer status polling

---
+++ Sources/TinkLink/Tasks/TransferStatusPollingTask.swift
@@ -0,0 +1,93 @@
+import Foundation
+
+// TODO: Abstruct this and credentials status polling into a common class
+final class TransferStatusPollingTask {
+    private let service: TransferService
+    private let updateHandler: (Result<SignableOperation, Error>) -> Void
+    private let retryInterval: TimeInterval = 1
+    private let applicationObserver = ApplicationObserver()
+
+    private var signableOperation: SignableOperation
+    private var callRetryCancellable: RetryCancellable?
+
+    private var isPaused = true
+    private var isActive = true
+
+    init(transferService: TransferService, signableOperation: SignableOperation, updateHandler: @escaping (Result<SignableOperation, Error>) -> Void) {
+        self.service = transferService
+        self.signableOperation = signableOperation
+        self.updateHandler = updateHandler
+
+        applicationObserver.didBecomeActive = { [weak self] in
+            guard let self = self, self.isActive == false else { return }
+            self.isActive = true
+            self.pollStatus()
+        }
+
+        applicationObserver.willResignActive = { [weak self] in
+            guard let self = self else { return }
+            self.isActive = false
+            self.callRetryCancellable?.cancel()
+            self.callRetryCancellable = nil
+        }
+    }
+
+    func startPolling() {
+
+        // Only start polling if we're not currently polling.
+        guard isPaused else {
+            return
+        }
+
+        isPaused = false
+        retryInterval = 1
+        DispatchQueue.main.asyncAfter(deadline: .now() + retryInterval) {
+            self.pollStatus()
+        }
+    }
+
+    private func pollStatus() {
+
+        guard let transferID = signableOperation.transferID else {
+            // TODO: Call handler with error if cannot find transferID
+            return
+        }
+
+        if isPaused || !isActive {
+            return
+        }
+
+        service.transferStatus(transferID: transferID) { [weak self] result in
+            guard let self = self else { return }
+            self.callRetryCancellable = nil
+            do {
+                let signableOperation = result.get()
+
+                defer {
+                    self.retry()
+                }
+
+                guard signableOperation.status != self.signableOperation.status {
+                    return
+                }
+
+                self.signableOperation = signableOperation
+                self.updateHandler(.success(signableOperation))
+            } catch {
+                self.updateHandler(.failure(error))
+            }
+        }
+    }
+
+    private func retry() {
+        if isPaused { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + retryInterval) { [weak self] in
+            self?.pollStatus()
+        }
+    }
+
+    func stopPolling() {
+        callRetryCancellable?.cancel()
+        isPaused = true
+    }
+}
