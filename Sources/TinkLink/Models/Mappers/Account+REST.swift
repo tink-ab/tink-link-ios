045c6649569b63eac79cdf81663ee02d8fa80b3f
Menghao Zhang <menghao.zhang@tink.se>
2020-05-11 15:40:58 +0200

    Update the variable naming factory method

--- Sources/TinkLink/Models/Mappers/Account+REST.swift
+++ Sources/TinkLink/Models/Mappers/Account+REST.swift
@@ -10,25 +10,25 @@ extension Account {
         self.name = account.name
         self.ownership = account.ownership
         self.kind = Account.Kind(restAccountType: account.type)
-        self.identifiers = Self.makeTransferEntityURI(account.identifiers)
+        self.identifiers = Account.makeTransferEntityURI(identifiers: account.identifiers)
         self.transferDestinations = account.transferDestinations?.compactMap { TransferDestination(restTransferDestination: $0) }
         self.details = account.details.flatMap { AccountDetails(restAccountDetails: $0) }
         self.holderName = account.holderName
         self.isClosed = account.closed
-        self.flags = Account.makeAccountFlags(account.flags)
-        self.accountExclusion = Self.AccountExclusion(restAccountExclusion: account.accountExclusion)
+        self.flags = Account.makeAccountFlags(flags: account.flags)
+        self.accountExclusion = AccountExclusion(restAccountExclusion: account.accountExclusion)
         self.currencyDenominatedBalance = account.currencyDenominatedBalance.flatMap { CurrencyDenominatedAmount(restCurrencyDenominatedAmount: $0) }
         self.refreshed = account.refreshed
         self.financialInstitutionID = account.financialInstitutionId.flatMap { Provider.FinancialInstitution.ID($0) }
     }
 
-    static func makeAccountFlags(_ string: String?) -> [Account.Flag]? {
-        guard let data = string?.data(using: .utf8), let restAccountFlags = try? JSONDecoder().decode([RESTAccount.Flag].self, from: data) else { return nil }
+    static func makeAccountFlags(flags: String?) -> [Account.Flag]? {
+        guard let data = flags?.data(using: .utf8), let restAccountFlags = try? JSONDecoder().decode([RESTAccount.Flag].self, from: data) else { return nil }
         return restAccountFlags.compactMap { Account.Flag(restAccountFlags: $0) }
     }
 
-    static func makeTransferEntityURI(_ string: String?) -> [Transfer.TransferEntityURI]? {
-        guard let data = string?.data(using: .utf8), let restIdentifiers = try? JSONDecoder().decode([String].self, from: data) else { return nil }
+    static func makeTransferEntityURI(identifiers: String?) -> [Transfer.TransferEntityURI]? {
+        guard let data = identifiers?.data(using: .utf8), let restIdentifiers = try? JSONDecoder().decode([String].self, from: data) else { return nil }
         return restIdentifiers.compactMap { Transfer.TransferEntityURI($0) }
     }
 }
