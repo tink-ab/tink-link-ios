8eb5f6787ee173121aeb935a2bb9eb3bd63b41be
Menghao Zhang <menghao.zhang@tink.se>
2020-05-11 11:04:56 +0200

    transferdestination maper

---
+++ Sources/TinkLink/Models/Mappers/TransferDestination+REST.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+extension TransferDestination {
+    init(restTransferDestination destination: RESTTransferDestination) {
+        self.balance = destination.balance.flatMap({ ExactNumber(value: Decimal($0)) })
+        self.displayAccountNumber = destination.displayAccountNumber
+        self.displayBankName = destination.displayBankName
+        self.kind = destination.type.flatMap({ TransferDestination.Kind(restTransferDestinationType: $0) }) ?? .unknown
+        self.matchesMultiple = destination.matchesMultiple
+        self.name = destination.name
+        self.uri = destination.uri.flatMap({ Transfer.TransferEntityURI($0) })
+    }
+}
+
+extension TransferDestination.Kind {
+    init(restTransferDestinationType type: RESTTransferDestination.ModelType) {
+        switch type {
+        case .checking: self = .checking
+        case .creditCard: self = .creditCard
+        case .external: self = .external
+        case .investment: self = .investment
+        case .loan: self = .loan
+        case .savings: self = .savings
+        }
+    }
+}
