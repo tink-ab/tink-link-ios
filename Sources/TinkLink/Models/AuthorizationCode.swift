8a7fecce39d48719796cfd66ed64e4659753d07a
Kasper Lahti <kasper.lahti@tink.se>
2020-05-13 08:16:30 +0200

    Let auth code be created with a string literal

--- Sources/TinkLink/Models/AuthorizationCode.swift
+++ Sources/TinkLink/Models/AuthorizationCode.swift
@@ -1,9 +1,13 @@
 /// An authorization code from the Tink backend.
-public struct AuthorizationCode: Hashable, RawRepresentable, Decodable {
+public struct AuthorizationCode: Hashable, RawRepresentable, Decodable, ExpressibleByStringLiteral {
     public let rawValue: String
 
     public init?(rawValue: String) {
         self.rawValue = rawValue
+    }
+
+    public init(stringLiteral value: String) {
+        self.rawValue = value
     }
 
     public init(_ value: String) {
