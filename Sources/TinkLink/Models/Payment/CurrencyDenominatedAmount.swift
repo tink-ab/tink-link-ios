21f85752d4dbf5e02a8fc786d712546142813b1a
Kasper Lahti <kasper.lahti@tink.se>
2020-05-14 09:26:08 +0200

    Don't need to specify value parameter

--- Sources/TinkLink/Models/Payment/CurrencyDenominatedAmount.swift
+++ Sources/TinkLink/Models/Payment/CurrencyDenominatedAmount.swift
@@ -6,12 +6,12 @@ public struct CurrencyDenominatedAmount: Equatable {
     /// The ISO 4217 currency code of the amount
     public let currencyCode: CurrencyCode
 
-    /// Creates a currency denominated amount with the value specified as an exact number.
+    /// Creates a currency denominated amount.
     ///
     /// - Parameters:
     ///   - value: The exact value of the amount.
     ///   - currencyCode: The exact value of the amount.
-    public init(value: ExactNumber, currencyCode: CurrencyCode) {
+    public init(_ value: ExactNumber, currencyCode: CurrencyCode) {
         self.value = value
         self.currencyCode = currencyCode
     }
