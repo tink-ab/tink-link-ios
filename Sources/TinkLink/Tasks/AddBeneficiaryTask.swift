56bd162290860df291ec489f1bf1c9f716cb2ef1
Kasper Lahti <kasper.lahti@tink.se>
2020-05-18 17:23:12 +0200

    Conform task to cancellable

--- Sources/TinkLink/Tasks/AddBeneficiaryTask.swift
+++ Sources/TinkLink/Tasks/AddBeneficiaryTask.swift
@@ -1,6 +1,10 @@
-public final class AddBeneficiaryTask {
+public final class AddBeneficiaryTask: Cancellable {
     public enum Authentication {
         case awaitingSupplementalInformation(SupplementInformationTask)
         case awaitingThirdPartyAppAuthentication(ThirdPartyAppAuthenticationTask)
+    }
+
+    public func cancel() {
+
     }
 }
