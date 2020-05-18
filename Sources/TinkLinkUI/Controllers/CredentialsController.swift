81d117959c1b23f95626823f4376f45e322ce37f
Pontus Orraryd <pontus.orraryd@tink.se>
2020-05-18 15:56:01 +0200

    Add credentials fetch to credentials controller

--- Sources/TinkLinkUI/Controllers/CredentialsController.swift
+++ Sources/TinkLinkUI/Controllers/CredentialsController.swift
@@ -9,6 +9,12 @@ final class CredentialsController {
         self.tink = tink
     }
 
+    func credentials(id: Credentials.ID, completion: @escaping (Result<Credentials, Error>) -> Void) {
+        tink._beginUITask()
+        defer { tink._endUITask() }
+        credentialsContext.fetchCredentials(with: id, completion: completion)
+    }
+
     func addCredentials(_ provider: Provider, form: Form, scopes: [Scope], progressHandler: @escaping (AddCredentialsTask.Status) -> Void, completion: @escaping (_ result: Result<Credentials, Error>) -> Void) -> AddCredentialsTask? {
         tink._beginUITask()
         return credentialsContext.add(
