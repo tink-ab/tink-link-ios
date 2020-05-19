6a66e0622d672ab0f38276947c3f4b777b9c4633
Menghao Zhang <menghao.zhang@tink.se>
2020-05-19 15:25:10 +0200

    Remove the beneficiary ID

--- Sources/TinkLink/Models/Mappers/Beneficiary+REST.swift
+++ Sources/TinkLink/Models/Mappers/Beneficiary+REST.swift
@@ -2,8 +2,6 @@ import Foundation
 
 extension Beneficiary {
     init(restBeneficiary beneficiary: RESTBeneficiary) {
-        // TODO: Change this
-        self.id = Beneficiary.ID(stringLiteral: beneficiary.name)
         self.accountID = Account.ID(stringLiteral: beneficiary.accountId)
         self.accountNumber = beneficiary.accountNumber
         self.name = beneficiary.name
