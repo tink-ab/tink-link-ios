2a58eeff24bf9a898f73a63d60b433a892c9ce76
Menghao Zhang <menghao.zhang@tink.se>
2020-05-22 13:25:40 +0200

    Copied the beneficiary definition from the unpublished doc

--- Sources/TinkLink/Models/Payment/Beneficiary.swift
+++ Sources/TinkLink/Models/Payment/Beneficiary.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/// A beneficiary for a user’s account
+/// A beneficiary is a payment or transfer destination account.
 public struct Beneficiary: Equatable {
     /// beneficiary type
     public let type: String
