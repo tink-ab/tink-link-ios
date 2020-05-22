339686239d428832d21644b87b8f9ded79a41f18
Menghao Zhang <menghao.zhang@tink.se>
2020-05-22 09:15:17 +0200

    Only append the name parameter when the name is available

--- Sources/TinkLink/Models/Payment/Beneficiary.swift
+++ Sources/TinkLink/Models/Payment/Beneficiary.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 public struct Beneficiary: Equatable {
+    public let type: String
     public let name: String?
     public let accountID: Account.ID
     public let accountNumber: String?
