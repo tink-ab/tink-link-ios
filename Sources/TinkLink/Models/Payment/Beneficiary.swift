eb7beb729322e4b70cd26521b595dc8e0243993c
Menghao Zhang <menghao.zhang@tink.se>
2020-05-22 10:41:27 +0200

    doc for beneficiary

--- Sources/TinkLink/Models/Payment/Beneficiary.swift
+++ Sources/TinkLink/Models/Payment/Beneficiary.swift
@@ -1,9 +1,14 @@
 import Foundation
 
+/// A beneficiary for a user’s account
 public struct Beneficiary: Equatable {
+    /// beneficiary type
     public let type: String
+    /// beneficiary name
     public let name: String?
+    /// Account ID that linked to hte beneficiary
     public let accountID: Account.ID
+    /// Account name that linked to hte beneficiary
     public let accountNumber: String?
 
     let uri: URL?
