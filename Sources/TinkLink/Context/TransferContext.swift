eda5cea9e1920b85b9edbba5170ee0a5ac401da0
Kasper Lahti <kasper.lahti@tink.se>
2020-05-22 16:00:10 +0200

    Whitespace

--- Sources/TinkLink/Context/TransferContext.swift
+++ Sources/TinkLink/Context/TransferContext.swift
@@ -1,4 +1,5 @@
 import Foundation
+
 /// An object that you use to access the user's account, beneficiaries and transfer functionality.
 public final class TransferContext {
     private let tink: Tink
