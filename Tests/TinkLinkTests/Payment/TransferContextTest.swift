273080047503734f43ef370ccabb2b38ba118b1a
Menghao Zhang <menghao.zhang@tink.se>
2020-05-26 13:54:39 +0200

    fetch beneficiaries test

--- Tests/TinkLinkTests/Payment/TransferContextTest.swift
+++ Tests/TinkLinkTests/Payment/TransferContextTest.swift
@@ -13,15 +13,34 @@ class TransferContextTests: XCTestCase {
         mockedUnauthenticatedErrorTransferService = MockedUnauthenticatedErrorTransferService()
     }
 
-    func testFetchAccount() {
-        let fetchAccountCompletionCalled = expectation(description: "fetch accounts completion should be called")
+    func testFetchAccounts() {
+        let fetchAccountsCompletionCalled = expectation(description: "fetch accounts completion should be called")
 
         mockedSuccessTransferService.accounts(destinationUris: []) { result in
             do {
                 _ = try result.get()
-                fetchAccountCompletionCalled.fulfill()
+                fetchAccountsCompletionCalled.fulfill()
             } catch {
                 XCTFail("Failed to fetch accounts with: \(error)")
+            }
+        }
+
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectations timeout with error: \(error)")
+            }
+        }
+    }
+
+    func testFetchBeneficiaries() {
+        let fetchBeneficiariesCompletionCalled = expectation(description: "fetch beneficiaries completion should be called")
+
+        mockedSuccessTransferService.beneficiaries { result in
+            do {
+                _ = try result.get()
+                fetchBeneficiariesCompletionCalled.fulfill()
+            } catch {
+                XCTFail("Failed to fetch beneficiaries with: \(error)")
             }
         }
 
