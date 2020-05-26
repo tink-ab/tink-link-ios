dc385ef8cd8e8263d69a98b8e56b7e2f28dafde8
Menghao Zhang <menghao.zhang@tink.se>
2020-05-26 13:54:29 +0200

    clean up

--- Tests/TinkLinkTests/Services+mock/TransferService+mock.swift
+++ Tests/TinkLinkTests/Services+mock/TransferService+mock.swift
@@ -11,36 +11,43 @@ class MockedSuccessTransferService: TransferService {
         }
     }
 
+    @discardableResult
     func beneficiaries(completion: @escaping (Result<[Beneficiary], Error>) -> Void) -> RetryCancellable? {
         completion(.success([Beneficiary.savingBeneficiary]))
         return nil
     }
 
+    @discardableResult
     func transfer(transfer: Transfer, redirectURI: URL, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable? {
         return nil
     }
 
+    @discardableResult
     func transferStatus(transferID: Transfer.ID, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable? {
         return nil
     }
 }
 
 class MockedUnauthenticatedErrorTransferService: TransferService {
+    @discardableResult
     func accounts(destinationUris: [URL], completion: @escaping (Result<[Account], Error>) -> Void) -> RetryCancellable? {
         completion(.failure(ServiceError.unauthenticatedError))
         return nil
     }
 
+    @discardableResult
     func beneficiaries(completion: @escaping (Result<[Beneficiary], Error>) -> Void) -> RetryCancellable? {
         completion(.failure(ServiceError.unauthenticatedError))
         return nil
     }
 
+    @discardableResult
     func transfer(transfer: Transfer, redirectURI: URL, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable? {
         completion(.failure(ServiceError.unauthenticatedError))
         return nil
     }
 
+    @discardableResult
     func transferStatus(transferID: Transfer.ID, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable? {
         completion(.failure(ServiceError.unauthenticatedError))
         return nil
