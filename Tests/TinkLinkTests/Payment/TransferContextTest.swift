538df8afba105d9b8841c85ec5bead05caf4393e
Menghao Zhang <menghao.zhang@tink.se>
2020-05-27 15:14:19 +0200

    add test for transfer getting cancelled

--- Tests/TinkLinkTests/Payment/TransferContextTest.swift
+++ Tests/TinkLinkTests/Payment/TransferContextTest.swift
@@ -4,6 +4,7 @@ import XCTest
 
 class TransferContextTests: XCTestCase {
     var mockedSuccessTransferService: MockedSuccessTransferService!
+    var mockedCancelledTransferService: MockedCancelledTransferService!
     var mockedUnauthenticatedErrorTransferService: MockedUnauthenticatedErrorTransferService!
 
     var mockedSuccessCredentialsService: MockedSuccessCredentialsServiceForPayment!
@@ -15,7 +16,9 @@ class TransferContextTests: XCTestCase {
         try! Tink.configure(with: .init(clientID: "testID", redirectURI: URL(string: "app://callback")!))
 
         mockedSuccessTransferService = MockedSuccessTransferService()
+        mockedCancelledTransferService = MockedCancelledTransferService()
         mockedUnauthenticatedErrorTransferService = MockedUnauthenticatedErrorTransferService()
+
         mockedSuccessCredentialsService = MockedSuccessCredentialsServiceForPayment()
         mockedUnauthenticatedErrorCredentialsService = MockedUnauthenticatedErrorCredentialsService()
     }
@@ -96,6 +99,60 @@ class TransferContextTests: XCTestCase {
                 initiateTransferCompletionCalled.fulfill()
             } catch {
                 XCTFail("Failed to initiate transfer with: \(error)")
+            }
+        }
+
+        waitForExpectations(timeout: 10) { error in
+            if let error = error {
+                XCTFail("waitForExpectations timeout with error: \(error)")
+            }
+        }
+    }
+
+    func testInitiateTransferThatGetCancelled() {
+        let transferContext = TransferContext(tink: .shared, transferService: mockedCancelledTransferService, credentialsService: mockedSuccessCredentialsService)
+        let statusChangedToCreated = expectation(description: "initiate transfer status should be changed to created")
+        let statusChangedToAuthenticating = expectation(description: "initiate transfer status should be changed to created")
+        let statusChangedToAwaitingSupplementalInformation = expectation(description: "initiate transfer status should be changed to awaitingSupplementalInformation")
+        let initiateTransferCancelledCalled = expectation(description: "initiate transfer completion with cancelled error should be called")
+
+        task = transferContext.initiateTransfer(
+            from: Account.checkingTestAccount,
+            to: Beneficiary.savingBeneficiary,
+            amount: CurrencyDenominatedAmount(10, currencyCode: CurrencyCode("EUR")),
+            destinationMessage: "test",
+            authentication: { task in
+                switch task {
+                case .awaitingThirdPartyAppAuthentication: break
+                case .awaitingSupplementalInformation(let supplementInformationTask):
+                    let form = Form(credentials: supplementInformationTask.credentials)
+                    supplementInformationTask.submit(form)
+                    statusChangedToAwaitingSupplementalInformation.fulfill()
+                }
+        },
+            progress: { status in
+                switch status {
+                case .created:
+                    statusChangedToCreated.fulfill()
+                case .authenticating:
+                    statusChangedToAuthenticating.fulfill()
+                default:
+                    break
+                }
+        }
+        ) { result in
+            do {
+                _ = try result.get()
+                XCTFail("Initiate transfer should be cancelled")
+            } catch {
+                if let initiateTransferTaskError = error as? InitiateTransferTask.Error {
+                    switch initiateTransferTaskError {
+                    case .cancelled:
+                        initiateTransferCancelledCalled.fulfill()
+                    default:
+                        XCTFail("Failed to initiate transfer with: \(error)")
+                    }
+                }
             }
         }
 
