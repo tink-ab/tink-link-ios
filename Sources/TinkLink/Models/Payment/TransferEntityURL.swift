601948372eea143e8ed943b702f464c54054a386
Kasper Lahti <kasper.lahti@tink.se>
2020-05-27 10:46:28 +0200

    Document the kind type

--- Sources/TinkLink/Models/Payment/TransferEntityURL.swift
+++ Sources/TinkLink/Models/Payment/TransferEntityURL.swift
@@ -3,15 +3,19 @@ import Foundation
 /// A TransferEntityURI represents the URI for making or reciving transfers.
 /// A TransferEntityURI is composed with two parts, a kind with value of e.g. `iban` and an account number.
 public struct TransferEntityURI {
+    /// A type representing an kind of account URI.
     public struct Kind: ExpressibleByStringLiteral {
         public init(stringLiteral value: String) {
             self.value = value
         }
 
+        /// Creates a kind.
+        /// - Parameter value: The `String` that represents the account kind.
         public init(_ value: String) {
             self.value = value
         }
 
+        /// The `String` that represent the account kind.
         public let value: String
     }
 
