28711cf0f533bbfa7147082157b6408e93fba623
Kasper Lahti <kasper.lahti@tink.se>
2020-05-27 16:05:36 +0200

    Create decimal from exact number

--- Sources/TinkLink/Models/Mappers/CurrencyDenominatedAmount+REST.swift
+++ Sources/TinkLink/Models/Mappers/CurrencyDenominatedAmount+REST.swift
@@ -2,7 +2,11 @@ import Foundation
 
 extension CurrencyDenominatedAmount {
     init(restCurrencyDenominatedAmount amount: RESTCurrencyDenominatedAmount) {
-        self.value = ExactNumber(unscaledValue: amount.unscaledValue, scale: Int64(amount.scale))
+        self.value = Decimal(
+            sign: amount.unscaledValue < 0 ? .minus : .plus,
+            exponent: -amount.scale,
+            significand: Decimal(amount.unscaledValue)
+        )
         self.currencyCode = CurrencyCode(amount.currencyCode)
     }
 }
