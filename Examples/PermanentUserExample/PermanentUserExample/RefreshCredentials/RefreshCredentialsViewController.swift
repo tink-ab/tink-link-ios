ccf783f4a854c2875566ccee0d3e77305ccef7e5
Kasper Lahti <kasper.lahti@tink.se>
2020-05-27 18:31:29 +0200

    Remove duplicate show alert for error methods

--- Examples/PermanentUserExample/PermanentUserExample/RefreshCredentials/RefreshCredentialsViewController.swift
+++ Examples/PermanentUserExample/PermanentUserExample/RefreshCredentials/RefreshCredentialsViewController.swift
@@ -299,20 +299,6 @@ extension RefreshCredentialsViewController {
         let navigationController = UINavigationController(rootViewController: supplementalInformationViewController)
         show(navigationController, sender: nil)
     }
-
-    private func showAlert(for error: Error) {
-        let localizedError = error as? LocalizedError
-        let alertController = UIAlertController(
-            title: localizedError?.errorDescription ?? "Error",
-            message: localizedError?.failureReason ?? error.localizedDescription,
-            preferredStyle: .alert
-        )
-
-        let okAction = UIAlertAction(title: "OK", style: .default)
-        alertController.addAction(okAction)
-
-        present(alertController, animated: true)
-    }
 }
 
 // MARK: - SupplementalInformationViewControllerDelegate
