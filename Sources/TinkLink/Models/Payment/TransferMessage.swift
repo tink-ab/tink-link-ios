960fd4f1967347c82ce0ccdf2f39eeaf04c2c926
Kasper Lahti <kasper.lahti@tink.se>
2020-05-27 14:06:30 +0200

    Nest message type in transfer task

--- Sources/TinkLink/Models/Payment/TransferMessage.swift
+++ Sources/TinkLink/Models/Payment/TransferMessage.swift
@@ -1,18 +1,20 @@
-/// Message for a transfer, composed of a source and destination message.
-public struct TransferMessage {
-    /// Optional, The transfer description on the source account for the transfer.
-    public var source: String?
+extension InitiateTransferTask {
+    /// Message for a transfer, composed of a source and destination message.
+    public struct Message {
+        /// Optional, The transfer description on the source account for the transfer.
+        public var source: String?
 
-    /// The message to the recipient. If the payment recipient requires a structured (specially formatted) message, it should be set in this field.
-    public var destination: String
+        /// The message to the recipient. If the payment recipient requires a structured (specially formatted) message, it should be set in this field.
+        public var destination: String
 
-    /// Creates a transfer message.
-    ///
-    /// - Parameters:
-    ///   - source: Optional, The transfer description on the source account for the transfer.
-    ///   - destination: The message to the recipient. If the payment recipient requires a structured (specially formatted) message, it should be set in this field.
-    public init(source: String? = nil, destination: String) {
-        self.source = source
-        self.destination = destination
+        /// Creates a transfer message.
+        ///
+        /// - Parameters:
+        ///   - source: Optional, The transfer description on the source account for the transfer.
+        ///   - destination: The message to the recipient. If the payment recipient requires a structured (specially formatted) message, it should be set in this field.
+        public init(source: String? = nil, destination: String) {
+            self.source = source
+            self.destination = destination
+        }
     }
 }
