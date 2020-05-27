6242613efc8d597360941e71e89a698e47bfb0e3
Kasper Lahti <kasper.lahti@tink.se>
2020-05-27 12:03:22 +0200

    Add documentation comments

--- Sources/TinkLink/Models/Payment/TransferMessage.swift
+++ Sources/TinkLink/Models/Payment/TransferMessage.swift
@@ -1,7 +1,16 @@
+/// Message for a transfer, composed of a source and destination message.
 public struct TransferMessage {
+    /// Optional, The transfer description on the source account for the transfer.
     public var sourceMessage: String?
+
+    /// The message to the recipient. If the payment recipient requires a structured (specially formatted) message, it should be set in this field.
     public var destinationMessage: String
 
+    /// Creates a transfer message.
+    ///
+    /// - Parameters:
+    ///   - sourceMessage: Optional, The transfer description on the source account for the transfer.
+    ///   - destinationMessage: The message to the recipient. If the payment recipient requires a structured (specially formatted) message, it should be set in this field.
     public init(sourceMessage: String? = nil, destinationMessage: String) {
         self.sourceMessage = sourceMessage
         self.destinationMessage = destinationMessage
