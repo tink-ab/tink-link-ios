034d0f98a9f99cdb6f48519c96ffb39e670f917c
Kasper Lahti <kasper.lahti@tink.se>
2020-05-28 12:23:42 +0200

    Use explanations from api docs

--- Sources/TinkLink/Models/Payment/Beneficiary.swift
+++ Sources/TinkLink/Models/Payment/Beneficiary.swift
@@ -2,13 +2,13 @@ import Foundation
 
 /// A beneficiary is a payment or transfer destination account.
 public struct Beneficiary: Equatable {
-    /// The beneficiary type
+    /// The type of the accountNumber that this beneficiary has.
     public let type: String
-    /// The beneficiary name
+    /// The name chosen by the user for this beneficiary.
     public let name: String?
-    /// Account ID that's associated with this beneficiary.
+    /// The identifier of the account that this beneficiary belongs to.
     public let accountID: Account.ID
-    /// Account number that's associated with this beneficiary.
+    /// The account number for the beneficiary. The structure of this field depends on the type.
     public let accountNumber: String?
 
     let uri: URL?
