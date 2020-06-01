315e0abba156322d3ef9a1037735de924a823b46
Menghao Zhang <menghao.zhang@tink.se>
2020-06-01 11:05:35 +0200

    removed the redundant show alert

--- Examples/PermanentUserExample/PermanentUserExample/RefreshCredentials/RefreshCredentialsViewController.swift
+++ Examples/PermanentUserExample/PermanentUserExample/RefreshCredentials/RefreshCredentialsViewController.swift
@@ -343,18 +343,6 @@ extension RefreshCredentialsViewController {
         dismiss(animated: animated, completion: completion)
         statusViewController = nil
     }
-
-    private func showAlert(for error: Error) {
-        let localizedError = error as? LocalizedError
-        let alertController = UIAlertController(
-            title: localizedError?.errorDescription ?? "Error",
-            message: localizedError?.failureReason ?? error.localizedDescription,
-            preferredStyle: .alert
-        )
-
-        let okAction = UIAlertAction(title: "OK", style: .default)
-        alertController.addAction(okAction)
-    }
 }
 
 // MARK: - SupplementalInformationViewControllerDelegate
