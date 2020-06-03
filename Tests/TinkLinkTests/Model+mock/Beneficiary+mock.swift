685781dd991f4dc07ff29afd15b3015b4f66ed11
Kasper Lahti <kasper.lahti@tink.se>
2020-06-03 16:06:53 +0200

    Update test for new parameter ordering

--- Tests/TinkLinkTests/Model+mock/Beneficiary+mock.swift
+++ Tests/TinkLinkTests/Model+mock/Beneficiary+mock.swift
@@ -5,8 +5,8 @@ import XCTest
 extension Beneficiary {
     static let savingBeneficiary = Beneficiary(
         accountNumberKind: "se",
+        accountNumber: "254fa71273394c5890de54fb3d20ac0f",
         name: "Savings Account tink",
-        ownerAccountID: "1078646804708704",
-        accountNumber: "254fa71273394c5890de54fb3d20ac0f"
+        ownerAccountID: "1078646804708704"
     )
 }
