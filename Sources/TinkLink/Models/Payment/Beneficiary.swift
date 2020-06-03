f78cc279b9b961b5973b63649fa80b2df5504335
Kasper Lahti <kasper.lahti@tink.se>
2020-06-03 15:55:51 +0200

    Use account number kind in beneficiary model

--- Sources/TinkLink/Models/Payment/Beneficiary.swift
+++ Sources/TinkLink/Models/Payment/Beneficiary.swift
@@ -6,7 +6,10 @@ import Foundation
 /// - Note: Different banks treat beneficiaries in different ways. Some treat them as fully trusted, meaning no signing at all is required when transferring money to the beneficiary. Other banks treat them more as address books of registered recipients
 public struct Beneficiary: Equatable {
     /// The type of the accountNumber that this beneficiary has.
-    public let accountNumberType: String
+    @available(*, deprecated, renamed: "accountNumberKind")
+    public var accountNumberType: String { accountNumberKind.value }
+    /// The type of the accountNumber that this beneficiary has.
+    public let accountNumberKind: AccountNumberKind
     /// The name chosen by the user for this beneficiary.
     public let name: String
     /// The identifier of the account that this beneficiary belongs to.
