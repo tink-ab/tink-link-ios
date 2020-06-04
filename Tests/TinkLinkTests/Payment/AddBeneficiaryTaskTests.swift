03c460512492e2c3d239a50e2962ba9bd4c65176
Kasper Lahti <kasper.lahti@tink.se>
2020-06-04 15:40:26 +0200

    Add test for adding beneficiary with different credentials than account

--- Tests/TinkLinkTests/Payment/AddBeneficiaryTaskTests.swift
+++ Tests/TinkLinkTests/Payment/AddBeneficiaryTaskTests.swift
@@ -188,4 +188,67 @@ class AddBeneficiaryTaskTests: XCTestCase {
             }
         }
     }
+
+    func testAddBeneficiaryTaskWithDifferentCredentialsThanAccount() {
+        let credentialsWithoutCapability = Credentials.makeTestCredentials(
+            providerID: "test-provider-a",
+            kind: .password,
+            status: .updated
+        )
+
+        let credentialsWithCapability = Credentials.makeTestCredentials(
+            providerID: "test-provider-b",
+            kind: .password,
+            status: .updated
+        )
+
+        let credentialsService = MutableCredentialsService(credentialsList: [credentialsWithoutCapability, credentialsWithCapability])
+
+        let account = Account.makeTestAccount(credentials: credentialsWithoutCapability)
+
+        XCTAssertEqual(account.credentialsID, credentialsWithoutCapability.id)
+        XCTAssertNotEqual(account.credentialsID, credentialsWithCapability.id)
+
+        let transferContext = TransferContext(tink: .shared, transferService: mockedSuccessTransferService, credentialsService: credentialsService)
+
+        let statusChangedToRequestSent = expectation(description: "add beneficiary status should be changed to created")
+        let statusChangedToAuthenticating = expectation(description: "add beneficiary status should be changed to created")
+        let addBeneficiaryCompletionCalled = expectation(description: "add beneficiary completion should be called")
+
+
+        task = transferContext.addBeneficiary(
+            name: "Example Inc",
+            accountNumberKind: .iban,
+            accountNumber: "FR7630006000011234567890189",
+            toAccountWithID: account.id,
+            onCredentialsWithID: credentialsWithCapability.id,
+            authentication: { task in
+                XCTFail("Didn't expect an authentication task")
+            },
+            progress: { status in
+                switch status {
+                case .requestSent:
+                    statusChangedToRequestSent.fulfill()
+                    credentialsService.modifyCredentials(id: credentialsWithCapability.id, status: .authenticating)
+                case .authenticating:
+                    statusChangedToAuthenticating.fulfill()
+                    credentialsService.modifyCredentials(id: credentialsWithCapability.id, status: .updated)
+                }
+            },
+            completion: { result in
+                do {
+                    _ = try result.get()
+                } catch {
+                    XCTFail("Failed to add beneficiary with: \(error)")
+                }
+                addBeneficiaryCompletionCalled.fulfill()
+            }
+        )
+
+        waitForExpectations(timeout: 10) { error in
+            if let error = error {
+                XCTFail("waitForExpectations timeout with error: \(error)")
+            }
+        }
+    }
 }
