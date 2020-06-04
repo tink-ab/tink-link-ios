4bc2dde49e295f949bf0fe0703831c1ac023bb9e
Kasper Lahti <kasper.lahti@tink.se>
2020-06-04 15:24:26 +0200

    Fix code formatting change

--- Tests/TinkLinkTests/Payment/AddBeneficiaryTaskTests.swift
+++ Tests/TinkLinkTests/Payment/AddBeneficiaryTaskTests.swift
@@ -55,7 +55,7 @@ class AddBeneficiaryTaskTests: XCTestCase {
                     statusChangedToAuthenticating.fulfill()
                     credentialsService.modifyCredentials(id: credentials.id, status: .awaitingSupplementalInformation, supplementalInformationFields: [])
                 }
-            }
+            },
             completion: { result in
                 do {
                     _ = try result.get()
