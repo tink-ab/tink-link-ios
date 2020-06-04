d27fdfa2df39bebef4498ab67549c67fd91bdf6f
Kasper Lahti <kasper.lahti@tink.se>
2020-06-04 17:40:22 +0200

    Use same authentication task as add beneficiary task

--- Sources/TinkLink/Tasks/InitiateTransferTask.swift
+++ Sources/TinkLink/Tasks/InitiateTransferTask.swift
@@ -21,14 +21,7 @@ public final class InitiateTransferTask: Cancellable {
     /// Represents an authentication that needs to be completed by the user.
     ///
     /// - Note: Each case have an associated task which need to be completed by the user to continue the transfer initiation process.
-    public enum AuthenticationTask {
-        /// Indicates that there is additional information required from the user to proceed.
-        ///
-        /// This can for example be an OTP sent via SMS or authetication app.
-        case awaitingSupplementalInformation(SupplementInformationTask)
-        /// Indicates that there is an authentication in a third party app necessary to proceed with the authentication.
-        case awaitingThirdPartyAppAuthentication(ThirdPartyAppAuthenticationTask)
-    }
+    public typealias AuthenticationTask = TinkLink.AuthenticationTask
 
     /// Error that the `InitiateTransferTask` can throw.
     public enum Error: Swift.Error {
