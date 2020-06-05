5854bfa6f2f2b7de3e016ae11871be14a4f13804
Kasper Lahti <kasper.lahti@tink.se>
2020-06-05 08:45:32 +0200

    Docs formatting

--- Sources/TinkLink/Models/Payment/Beneficiary.swift
+++ Sources/TinkLink/Models/Payment/Beneficiary.swift
@@ -5,10 +5,10 @@ import Foundation
 /// Each beneficiary belongs to an account, which means that the given account can send money to that beneficiary.
 /// - Note: Different banks treat beneficiaries in different ways. Some treat them as fully trusted, meaning no signing at all is required when transferring money to the beneficiary. Other banks treat them more as address books of registered recipients
 public struct Beneficiary: Equatable, TransferBeneficiary {
-    /// The type of the accountNumber that this beneficiary has.
+    /// The type of the `accountNumber` that this beneficiary has.
     @available(*, deprecated, renamed: "accountNumberKind")
     public var accountNumberType: String { accountNumberKind.value }
-    /// The type of the accountNumber that this beneficiary has.
+    /// The type of the `accountNumber` that this beneficiary has.
     public let accountNumberKind: AccountNumberKind
     /// The account number for the beneficiary. The structure of this field depends on the `accountNumberKind`.
     public let accountNumber: String
