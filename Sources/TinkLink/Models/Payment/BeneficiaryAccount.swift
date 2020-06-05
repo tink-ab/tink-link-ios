5f2d3926a3f075a9da246259108e5594a5684300
Kasper Lahti <kasper.lahti@tink.se>
2020-06-05 08:50:21 +0200

    Conform to transfer beneficiary

--- Sources/TinkLink/Models/Payment/BeneficiaryAccount.swift
+++ Sources/TinkLink/Models/Payment/BeneficiaryAccount.swift
@@ -1,5 +1,5 @@
 /// A beneficiary account is a payment or transfer destination account.
-public struct BeneficiaryAccount {
+public struct BeneficiaryAccount: TransferBeneficiary {
     /// The type of the `accountNumber` that this beneficiary has.
     public let accountNumberKind: AccountNumberKind
     /// The account number for the beneficiary. The structure of this field depends on the `accountNumberKind`.
