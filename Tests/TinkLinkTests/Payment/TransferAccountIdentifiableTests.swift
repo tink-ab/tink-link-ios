b32848e72ab2ddc662c1cd16dc83fd45213c632a
Kasper Lahti <kasper.lahti@tink.se>
2020-06-08 16:30:55 +0200

    Test beneficiary conformance

--- Tests/TinkLinkTests/Payment/TransferAccountIdentifiableTests.swift
+++ Tests/TinkLinkTests/Payment/TransferAccountIdentifiableTests.swift
@@ -6,4 +6,9 @@ class TransferAccountIdentifiableTests: XCTestCase {
         let account = Account.checkingTestAccount
         XCTAssertEqual(account.transferAccountID, "iban://FR1420041010050015664355590?name=testAccount")
     }
+
+    func testBeneficiaryConformance() {
+        let beneficiary = Beneficiary.savingBeneficiary
+        XCTAssertEqual(beneficiary.transferAccountID, "se://254fa71273394c5890de54fb3d20ac0f")
+    }
 }
