45d715f2b570929bc3a06b6540ca3a28df02c170
Kasper Lahti <kasper.lahti@tink.se>
2020-06-08 15:44:54 +0200

    Conform account and beneficiary types to identifiable protocol

--- Sources/TinkLink/Models/Payment/TransferAccountIdentifiable.swift
+++ Sources/TinkLink/Models/Payment/TransferAccountIdentifiable.swift
@@ -5,3 +5,26 @@ public protocol TransferAccountIdentifiable {
     var transferAccountID: String { get }
 }
 
+extension Account: TransferAccountIdentifiable {
+    public var transferAccountID: String {
+        transferSourceIdentifiers?.first?.absoluteString ?? "tink://\(id.value)"
+    }
+}
+
+extension Beneficiary: TransferAccountIdentifiable {
+    public var transferAccountID: String {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = accountNumberKind.value
+        urlComponents.host = accountNumber
+        return urlComponents.url?.absoluteString ?? "\(accountNumberKind.value)://\(accountNumber)"
+    }
+}
+
+extension BeneficiaryAccount: TransferAccountIdentifiable {
+    public var transferAccountID: String {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = accountNumberKind.value
+        urlComponents.host = accountNumber
+        return urlComponents.url?.absoluteString ?? "\(accountNumberKind.value)://\(accountNumber)"
+    }
+}
