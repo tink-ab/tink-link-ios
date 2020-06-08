bc640ece93eabac0ede1e3fc3fb9339902f9766f
Kasper Lahti <kasper.lahti@tink.se>
2020-06-08 08:52:46 +0200

    Documentation comment formatting

--- Sources/TinkLink/Models/Payment/BeneficiaryAccountRepresentable.swift
+++ Sources/TinkLink/Models/Payment/BeneficiaryAccountRepresentable.swift
@@ -1,7 +1,7 @@
 /// A transfer beneficiary is a transfer destination account.
 public protocol BeneficiaryAccountRepresentable {
-    /// The type of the accountNumber that this beneficiary has.
+    /// The type of the `accountNumber` that this beneficiary has.
     var accountNumberKind: AccountNumberKind { get }
-    /// The account number for the beneficiary. The structure of this field depends on the type.
+    /// The account number for the beneficiary. The structure of this field depends on the `accountNumberKind`.
     var accountNumber: String { get }
 }
