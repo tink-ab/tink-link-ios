4aff65b2fef93ac039e7e3db4be02c533925e6e4
Kasper Lahti <kasper.lahti@tink.se>
2020-06-08 16:30:50 +0200

    Rename test

--- Tests/TinkLinkTests/Payment/TransferAccountIdentifiableTests.swift
+++ Tests/TinkLinkTests/Payment/TransferAccountIdentifiableTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import TinkLink
 
 class TransferAccountIdentifiableTests: XCTestCase {
-    func testAccount() {
+    func testAccountConformance() {
         let account = Account.checkingTestAccount
         XCTAssertEqual(account.transferAccountID, "iban://FR1420041010050015664355590?name=testAccount")
     }
