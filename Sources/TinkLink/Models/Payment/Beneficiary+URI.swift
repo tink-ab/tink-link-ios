a11bfb6907fa0a1c05957a2cccea30df01d52467
Kasper Lahti <kasper.lahti@tink.se>
2020-06-08 12:05:04 +0200

    Remove unused initializer

--- Sources/TinkLink/Models/Payment/Beneficiary+URI.swift
+++ Sources/TinkLink/Models/Payment/Beneficiary+URI.swift
@@ -63,14 +63,4 @@ extension Beneficiary.URI {
 
         self.value = uri.absoluteString
     }
-
-    /// Creates a URI for a transfer beneficiary.
-    /// - Parameter beneficiary: The transfer beneficiary.
-    public init?(beneficiary: AccountNumberRepresentable) {
-        var urlComponents = URLComponents()
-        urlComponents.scheme = beneficiary.accountNumberKind.value
-        urlComponents.host = beneficiary.accountNumber
-        guard let uri = urlComponents.url else { return nil }
-        self.value = uri.absoluteString
-    }
 }
