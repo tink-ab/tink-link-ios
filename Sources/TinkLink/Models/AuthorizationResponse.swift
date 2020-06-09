e89c654f76fa77a569dc88f893ae4cd59b00ab2e
Kasper Lahti <kasper.lahti@tink.se>
2020-06-09 15:14:42 +0200

    Remove decodable conformance from public model type

--- Sources/TinkLink/Models/AuthorizationResponse.swift
+++ Sources/TinkLink/Models/AuthorizationResponse.swift
@@ -1,4 +1,4 @@
 /// The response received when trying to authorize with the `AuthenticationService`.
 struct AuthorizationResponse: Decodable {
-    let code: AuthorizationCode
+    let code: String
 }
