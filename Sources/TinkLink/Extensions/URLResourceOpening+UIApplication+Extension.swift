1e7a1f146ef97397c454a84a268dec84d8b9666f
Menghao Zhang <menghao.zhang@tink.se>
2020-06-09 13:37:19 +0200

    updated the implementation

--- Sources/TinkLink/Extensions/URLResourceOpening+UIApplication+Extension.swift
+++ Sources/TinkLink/Extensions/URLResourceOpening+UIApplication+Extension.swift
@@ -8,4 +8,14 @@ protocol URLResourceOpening {
     var universalLinksOnlyOptionKey: String { get }
 }
 
-extension UIApplication: URLResourceOpening {}
+extension UIApplication: URLResourceOpening {
+    func open(_ url: URL, options: [String : Any], completionHandler completion: ((Bool) -> Void)?) {
+        let mappedOptions = options.map { (key, value) in
+            return (OpenExternalURLOptionsKey(rawValue: key), value)
+        }
+        let dictionary = Dictionary.init(uniqueKeysWithValues: mappedOptions)
+        open(url, options: dictionary, completionHandler: completion)
+    }
+
+    var universalLinksOnlyOptionKey: String { OpenExternalURLOptionsKey.universalLinksOnly.rawValue }
+}
--- Tests/TinkLinkTests/Model+mock/UIApplication+mock.swift
+++ Tests/TinkLinkTests/Model+mock/UIApplication+mock.swift
@@ -1,13 +1,13 @@
 import Foundation
-#if os(iOS)
-import UIKit
-#endif
+
 @testable import TinkLink
 
 struct MockApplication: URLResourceOpening {
-    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey : Any], completionHandler completion: ((Bool) -> Void)?) {
+    func open(_ url: URL, options: [String : Any], completionHandler completion: ((Bool) -> Void)?) {
         completion?(true)
     }
+
+    var universalLinksOnlyOptionKey: String = "Test"
 }
 
 extension ThirdPartyAppAuthenticationTask {
