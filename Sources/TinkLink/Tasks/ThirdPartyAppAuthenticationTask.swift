7e96b369cb1a0ee9577423ee9afbcafe453bc5eb
Menghao Zhang <menghao.zhang@tink.se>
2020-06-09 12:07:19 +0200

    Removed the unnecessary mock function

--- Sources/TinkLink/Tasks/ThirdPartyAppAuthenticationTask.swift
+++ Sources/TinkLink/Tasks/ThirdPartyAppAuthenticationTask.swift
@@ -301,9 +301,4 @@ public class ThirdPartyAppAuthenticationTask: Identifiable {
         let updatedUrl = url.absoluteString.replacingOccurrences(of: tinkBankIDRedirect, with: bankIDredirect)
         return URL(string: updatedUrl)!
     }
-
-    // For testing only
-    func _complete(with result: Result<Void, Swift.Error>) {
-        completionHandler(result)
-    }
 }
