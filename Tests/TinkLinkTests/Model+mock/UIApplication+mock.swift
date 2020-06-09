e3a65b5549725838724660c705bc2486d2a254cc
Menghao Zhang <menghao.zhang@tink.se>
2020-06-09 12:06:19 +0200

    Added a mock application to confirm to the protocol and added the method only for test

---
+++ Tests/TinkLinkTests/Model+mock/UIApplication+mock.swift
@@ -0,0 +1,20 @@
+import Foundation
+#if os(iOS)
+import UIKit
+#endif
+@testable import TinkLink
+
+struct MockUIApplication: URLResourceOpening {
+    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey : Any], completionHandler completion: ((Bool) -> Void)?) {
+        completion?(true)
+    }
+}
+
+extension ThirdPartyAppAuthenticationTask {
+    func handle(with application: URLResourceOpening, completion: @escaping (Result<Void, Swift.Error>) -> Void) {
+        openThirdPartyApp(with: application) { [weak self] result in
+            self?.completionHandler(result)
+            completion(result)
+        }
+    }
+}
