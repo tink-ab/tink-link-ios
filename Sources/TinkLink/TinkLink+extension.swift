22b591fad302db4e1eed8788593e33c504db9b5c
Kasper Lahti <kasper.lahti@tink.se>
2020-06-16 15:32:53 +0200

    Remove auth and create user methods from extension

--- Sources/TinkLink/TinkLink+extension.swift
+++ Sources/TinkLink/TinkLink+extension.swift
@@ -46,45 +46,4 @@ extension Tink {
 
         return true
     }
-
-    // MARK: - Authenticating a User
-
-    /// Authenticate a permanent user with authorization code.
-    ///
-    /// - Parameter authorizationCode: Authenticate with a `AuthorizationCode` that delegated from Tink to exchanged for a user object.
-    /// - Parameter completion: A result representing either a success or an error.
-    @discardableResult
-    public func authenticateUser(authorizationCode: AuthorizationCode, completion: @escaping (Result<Void, Swift.Error>) -> Void) -> RetryCancellable? {
-        return oAuthService.authenticate(code: authorizationCode, completion: { [weak self] result in
-            do {
-                let authenticateResponse = try result.get()
-                let accessToken = authenticateResponse.accessToken
-                self?.setCredential(.accessToken(accessToken.rawValue))
-                completion(.success)
-            } catch {
-                completion(.failure(error))
-            }
-        })
-    }
-
-    /// Create a user for a specific market and locale.
-    ///
-    /// :nodoc:
-    ///
-    /// - Parameter market: Register a `Market` for creating the user, will use the default market if nothing is provided.
-    /// - Parameter locale: Register a `Locale` for creating the user, will use the default locale in TinkLink if nothing is provided.
-    /// - Parameter completion: A result representing either a success or an error.
-    @discardableResult
-    public func _createTemporaryUser(for market: Market, locale: Locale = Tink.defaultLocale, completion: @escaping (Result<Void, Swift.Error>) -> Void) -> RetryCancellable? {
-        return oAuthService.createAnonymous(market: market, locale: locale, origin: nil) { [weak self] result in
-            let mappedResult = result.mapError { UserError(createTemporaryUserError: $0) ?? $0 }
-            do {
-                let accessToken = try mappedResult.get()
-                self?.setCredential(.accessToken(accessToken.rawValue))
-                completion(.success)
-            } catch {
-                completion(.failure(error))
-            }
-        }
-    }
 }
