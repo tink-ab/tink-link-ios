a4a9fa07429e8f029d5b4d2898c1db095ec89e53
Pontus Orraryd <pontus.orraryd@tink.se>
2020-06-24 16:50:05 +0200

    Run swift format

--- Sources/TinkLink/Tasks/ThirdPartyAppAuthenticationTask.swift
+++ Sources/TinkLink/Tasks/ThirdPartyAppAuthenticationTask.swift
@@ -187,7 +187,7 @@ public class ThirdPartyAppAuthenticationTask: Identifiable {
 
                                 completion(.failure(downloadRequiredError))
                             }
-                    })
+                        })
                     }
                 }
             }
