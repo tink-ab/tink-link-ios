25cfa9e9098da06ee441229b1f7a0e0c2fdf47fd
Kasper Lahti <kasper.lahti@tink.se>
2020-08-13 12:00:02 +0200

    Update mocks for auth service to match protocol

--- Tests/TinkLinkTests/Services+mock/OAuthService+mock.swift
+++ Tests/TinkLinkTests/Services+mock/OAuthService+mock.swift
@@ -7,7 +7,7 @@ class MockedSuccessOAuthService: OAuthService {
         return nil
     }
 
-    func authenticate(code: AuthorizationCode, completion: @escaping (Result<AccessToken, Error>) -> Void) -> RetryCancellable? {
+    func authenticate(clientID: String, code: AuthorizationCode, completion: @escaping (Result<AccessToken, Error>) -> Void) -> RetryCancellable? {
         let accessToken = AccessToken("accessToken")
         completion(.success(accessToken))
         return nil
@@ -20,7 +20,7 @@ class MockedInvalidArgumentFailurefulOAuthService: OAuthService {
         return nil
     }
 
-    func authenticate(code: AuthorizationCode, completion: @escaping (Result<AccessToken, Error>) -> Void) -> RetryCancellable? {
+    func authenticate(clientID: String, code: AuthorizationCode, completion: @escaping (Result<AccessToken, Error>) -> Void) -> RetryCancellable? {
         completion(.failure(ServiceError.invalidArgumentError))
         return nil
     }
@@ -32,7 +32,7 @@ class MockedUnauthenticatedErrorOAuthService: OAuthService {
         return nil
     }
 
-    func authenticate(code: AuthorizationCode, completion: @escaping (Result<AccessToken, Error>) -> Void) -> RetryCancellable? {
+    func authenticate(clientID: String, code: AuthorizationCode, completion: @escaping (Result<AccessToken, Error>) -> Void) -> RetryCancellable? {
         completion(.failure(ServiceError.unauthenticatedError))
         return nil
     }
