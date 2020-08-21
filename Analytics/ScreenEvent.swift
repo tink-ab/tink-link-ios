5b9cc7011e66561cd6d98accf72993ca759cf9d6
Pontus Orraryd <pontus.orraryd@tink.se>
2020-08-21 09:34:34 +0200

    Add ScreenEvent enum

---
+++ Analytics/ScreenEvent.swift
@@ -0,0 +1,11 @@
+enum ScreenEvent: String {
+    case error
+    case successScreen = "success_screen"
+    case supplementalInformationScreen = "supplemental_information_screen"
+    case providerSelectionScreen = "provider_selection_screen"
+    case authenticationUserTypeSelectionScreen = "authentication_user_type_selection_screen"
+    case financialInstitutionSelectionScreen = "financial_institution_selection_screen"
+    case accessTypeSelectionScreen = "access_type_selection_screen"
+    case credentialsTypeSelectionScreen = "credentials_type_selection_screen"
+    case submitCredentialsScreen = "submit_credentials_screen"
+}
