9794e5a8ee4cf86d2581e5b9506b57b2e702d524
Kasper Lahti <kasper.lahti@tink.se>
2020-08-26 21:18:37 +0200

    Break up long method declaration to multiple lines

--- Sources/TinkLinkUI/Controllers/CredentialsController.swift
+++ Sources/TinkLinkUI/Controllers/CredentialsController.swift
@@ -15,7 +15,13 @@ final class CredentialsController {
         credentialsContext.fetchCredentials(with: id, completion: completion)
     }
 
-    func addCredentials(_ provider: Provider, form: Form, refreshableItems: RefreshableItems = .all, progressHandler: @escaping (AddCredentialsTask.Status) -> Void, completion: @escaping (_ result: Result<Credentials, Error>) -> Void) -> AddCredentialsTask? {
+    func addCredentials(
+        _ provider: Provider,
+        form: Form,
+        refreshableItems: RefreshableItems = .all,
+        progressHandler: @escaping (AddCredentialsTask.Status) -> Void,
+        completion: @escaping (_ result: Result<Credentials, Error>) -> Void
+    ) -> AddCredentialsTask? {
         tink._beginUITask()
         return credentialsContext.add(
             for: provider,
