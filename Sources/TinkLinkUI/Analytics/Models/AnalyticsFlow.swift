ee287436ff8c40fe1c0470cb08e030d48381ebce
Pontus Orraryd <pontus.orraryd@tink.se>
2020-08-26 09:52:16 +0200

    Add AnalyticsFlow model

---
+++ Sources/TinkLinkUI/Analytics/Models/AnalyticsFlow.swift
@@ -0,0 +1,6 @@
+enum AnalyticsFlow: String {
+    case credentialsAuthenticate = "credentials_authenticate"
+    case credentialsAdd = "credentials_add"
+    case credentialsRefresh = "credentials_refresh"
+    case credentialsUpdate = "credentials_update"
+}
