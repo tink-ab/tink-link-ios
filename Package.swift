5d45e310587f2422596a0d96a397f2e5f00e9125
Kasper Lahti <kasper.lahti@tink.se>
2020-08-31 16:24:51 +0200

    Bump core version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/tink-ab/tink-core-ios", .exact("0.1.1"))
+        .package(url: "https://github.com/tink-ab/tink-core-ios", .exact("0.1.2"))
     ],
     targets: [
         .target(
