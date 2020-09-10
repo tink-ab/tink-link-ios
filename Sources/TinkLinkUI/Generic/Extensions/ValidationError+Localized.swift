f769918c9d83976e0667a19d6a09318e9deda402
Kasper Lahti <kasper.lahti@tink.se>
2020-09-10 13:15:03 +0200

    Move strings for validation errors

--- Sources/TinkLinkUI/Generic/Extensions/ValidationError+Localized.swift
+++ Sources/TinkLinkUI/Generic/Extensions/ValidationError+Localized.swift
@@ -7,11 +7,11 @@ extension Form.Field.ValidationError: LocalizedError {
         case .invalid(_, let reason):
             return reason
         case .maxLengthLimit(let fieldName, let maxLength):
-            return String(format: Strings.Credentials.Validation.maxLengthLimit, fieldName, maxLength)
+            return String(format: Strings.Field.Validation.maxLengthLimit, fieldName, maxLength)
         case .minLengthLimit(let fieldName, let minLength):
-            return String(format: Strings.Credentials.Validation.minLengthLimit, fieldName, minLength)
+            return String(format: Strings.Field.Validation.minLengthLimit, fieldName, minLength)
         case .requiredFieldEmptyValue(let fieldName):
-            return String(format: Strings.Credentials.Validation.requiredFieldEmptyValue, fieldName)
+            return String(format: Strings.Field.Validation.requiredFieldEmptyValue, fieldName)
         }
     }
 }
