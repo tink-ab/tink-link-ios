a0bb9fd714359f2faa0e90bb7813bddd46590c1d
Kasper Lahti <kasper.lahti@tink.se>
2020-09-15 09:39:00 +0200

    Add launch ui test

---
+++ Tests/TinkLinkUIUITests/TinkLinkUITests.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+class TinkPFMUITests: XCTestCase {
+    let app = XCUIApplication()
+
+    override func setUp() {
+        continueAfterFailure = false
+
+        app.launchEnvironment = [
+        ]
+    }
+
+    func testLaunch() {
+        app.launch()
+    }
+}
