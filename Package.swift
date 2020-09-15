2ecd5288ab586af6a18a05ee8cceed909cd165b3
Kasper Lahti <kasper.lahti@tink.se>
2020-09-15 16:35:40 +0200

    Update core version in package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/tink-ab/tink-core-ios", .exact("0.1.3"))
+        .package(url: "https://github.com/tink-ab/tink-core-ios", .exact("0.1.5"))
     ],
     targets: [
         .target(
