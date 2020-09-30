34acb528061b971f29a949a72142f513f3af0160
Pontus Orraryd <pontus.orraryd@tink.se>
2020-09-30 15:09:53 +0200

    Add option to add credentials without passing a complete Provider object

--- Sources/TinkLink/Context/CredentialsContext.swift
+++ Sources/TinkLink/Context/CredentialsContext.swift
@@ -58,7 +58,7 @@ public final class CredentialsContext {
     ///
     /// You need to handle status changes in `progressHandler` to successfuly add a credentials for some providers.
     ///
-    ///     let addCredentialsTask = credentialsContext.add(for: provider, form: form, progressHandler: { status in
+    ///     let addCredentialsTask = credentialsContext.add(forProviderWithName: providerName, form: form, progressHandler: { status in
     ///         switch status {
     ///         case .awaitingSupplementalInformation(let supplementInformationTask):
     ///             <#Present form for supplemental information task#>
@@ -72,7 +72,7 @@ public final class CredentialsContext {
     ///     }
     ///
     /// - Parameters:
-    ///   - provider: The provider (financial institution) that the credentials is connected to.
+    ///   - providerName: The provider (financial institution) that the credentials is connected to.
     ///   - form: This is a form with fields from the Provider to which the credentials belongs to.
     ///   - refreshableItems: The data types to aggregate from the provider. Defaults to all types.
     ///   - completionPredicate: Predicate for when credentials task should complete.
@@ -82,7 +82,7 @@ public final class CredentialsContext {
     ///   - result: Represents either a successfully added credentials or an error if adding the credentials failed.
     /// - Returns: The add credentials task.
     public func add(
-        for provider: Provider,
+        forProviderWithName providerName: Provider.Name,
         form: Form,
         refreshableItems: RefreshableItems = .all,
         completionPredicate: AddCredentialsTask.CompletionPredicate = .init(successPredicate: .updated, shouldFailOnThirdPartyAppAuthenticationDownloadRequired: true),
@@ -90,8 +90,6 @@ public final class CredentialsContext {
         progressHandler: @escaping (_ status: AddCredentialsTask.Status) -> Void = { _ in },
         completion: @escaping (_ result: Result<Credentials, Error>) -> Void
     ) -> AddCredentialsTask {
-        let refreshableItems = refreshableItems.supporting(providerCapabilities: provider.capabilities)
-
         let task = AddCredentialsTask(
             credentialsService: service,
             completionPredicate: completionPredicate,
@@ -124,6 +122,49 @@ public final class CredentialsContext {
             }
         }
         return task
+    }
+
+    /// Adds a credentials for the user.
+    ///
+    /// Required scopes:
+    /// - credentials:write
+    ///
+    /// You need to handle status changes in `progressHandler` to successfuly add a credentials for some providers.
+    ///
+    ///     let addCredentialsTask = credentialsContext.add(for: provider, form: form, progressHandler: { status in
+    ///         switch status {
+    ///         case .awaitingSupplementalInformation(let supplementInformationTask):
+    ///             <#Present form for supplemental information task#>
+    ///         case .awaitingThirdPartyAppAuthentication(let thirdPartyAppAuthentication):
+    ///             <#Open third party app deep link URL#>
+    ///         default:
+    ///             break
+    ///         }
+    ///     }, completion: { result in
+    ///         <#Handle result#>
+    ///     }
+    ///
+    /// - Parameters:
+    ///   - provider: The provider (financial institution) that the credentials is connected to.
+    ///   - form: This is a form with fields from the Provider to which the credentials belongs to.
+    ///   - refreshableItems: The data types to aggregate from the provider. Defaults to all types.
+    ///   - completionPredicate: Predicate for when credentials task should complete.
+    ///   - progressHandler: The block to execute with progress information about the credential's status.
+    ///   - status: Indicates the state of a credentials being added.
+    ///   - completion: The block to execute when the credentials has been added successfuly or if it failed.
+    ///   - result: Represents either a successfully added credentials or an error if adding the credentials failed.
+    /// - Returns: The add credentials task.
+    public func add(
+        for provider: Provider,
+        form: Form,
+        refreshableItems: RefreshableItems = .all,
+        completionPredicate: AddCredentialsTask.CompletionPredicate = .init(successPredicate: .updated, shouldFailOnThirdPartyAppAuthenticationDownloadRequired: true),
+        progressHandler: @escaping (_ status: AddCredentialsTask.Status) -> Void,
+        completion: @escaping (_ result: Result<Credentials, Error>) -> Void
+    ) -> AddCredentialsTask {
+        let refreshableItems = refreshableItems.supporting(providerCapabilities: provider.capabilities)
+
+        return add(forProviderWithName: provider.name, form: form, refreshableItems: refreshableItems, completionPredicate: completionPredicate, progressHandler: progressHandler, completion: completion)
     }
 
     /// Fetch a list of the current user's credentials.
