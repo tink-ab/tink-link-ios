07eea15a315fcfe2db6e2ecbc0deb95c248823ce
Menghao Zhang <menghao.zhang@tink.se>
2020-10-06 16:53:28 +0200

    Updated protocol for color

--- Sources/TinkLinkUI/Appearance/ColorProviding.swift
+++ Sources/TinkLinkUI/Appearance/ColorProviding.swift
@@ -2,22 +2,32 @@ import UIKit
 
 /// A type that can provide colors for Tink views.
 public protocol ColorProviding {
+    /// Colors for indicators and other similar elements.
+    var accent: UIColor { get }
+    /// Colors for indicators and other similar elements background.
+    var accentBackground: UIColor { get }
     /// Color for the main background of the interface.
     var background: UIColor { get }
-    /// Color for content layered on top of the main background.
-    var secondaryBackground: UIColor { get }
-    /// Color for the main background of grouped interface components.
-    var groupedBackground: UIColor { get }
-    /// Color for content layered on top of the main background of grouped interface components.
-    var secondaryGroupedBackground: UIColor { get }
     /// Primary text color.
     var label: UIColor { get }
     /// Secondary text color.
     var secondaryLabel: UIColor { get }
     /// Color for separators.
     var separator: UIColor { get }
-    /// Colors for buttons, indicators and other similar elements.
-    var accent: UIColor { get }
+    /// Color for content layered on top of the main background.
+    var secondaryBackground: UIColor { get }
+
+    /// Color for buttons background.
+    var button: UIColor { get }
+    /// Color for buttons text.
+    var buttonText: UIColor { get }
+
+    /// Color for the main background of grouped interface components.
+    @available(*, deprecated, message: "Use accentBackground to update elements background")
+    var groupedBackground: UIColor { get }
+    /// Color for content layered on top of the main background of grouped interface components.
+    @available(*, deprecated, message: "Use accentBackground to update elements background")
+    var secondaryGroupedBackground: UIColor { get }
 
     // Semantic colors:
     /// Color representing a warning.
