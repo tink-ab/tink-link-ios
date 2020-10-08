6256ec10af11de1ea81a17d67fb854ba390aba07
Menghao Zhang <menghao.zhang@tink.se>
2020-10-08 11:53:00 +0200

    Added a script for update the debug mode core xcframework and use it for doing the test

---
+++ scripts/updateCoreForTest.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -rf ./tink-core-ios-private
+
+git clone -v git@github.com:tink-ab/tink-core-ios-private.git
+cd ./tink-core-ios-private
+
+make internal-distribution-framework
+cp -r ./build/TinkCore-internal.xcframework ./TinkCore.xcframework
+cd ..
+
+old_path=".package(name: \"TinkCore\", url: \"https://github.com/tink-ab/tink-core-ios\", .upToNextMajor(from: \"0.1.6\")),"
+new_path=".package(name: \"TinkCore\", path: \"../tink-core-ios-private\"),"
+sed -i '' "s|$old_path|$new_path|" Package.swift
