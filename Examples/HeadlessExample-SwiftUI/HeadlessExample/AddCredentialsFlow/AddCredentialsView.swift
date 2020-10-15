47e0f7cfcd51aa437c6b8a2e507a648be37eb4ca
Kasper Lahti <kasper.lahti@tink.se>
2020-10-15 09:25:15 +0200

    Wrap add credentials view controller

--- Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/AddCredentialsView.swift
+++ Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/AddCredentialsView.swift
@@ -1,11 +1,31 @@
 import SwiftUI
 import TinkLink
 
-struct AddCredentialsView: View {
+struct AddCredentialsView: View, UIViewControllerRepresentable {
     var provider: Provider
 
-    var body: some View {
-        Text(provider.displayName)
+    @EnvironmentObject var credentialsController: CredentialsController
+    @SwiftUI.Environment(\.presentationMode) var presentationMode
+
+    class Coordinator {}
+
+    func makeCoordinator() -> AddCredentialsView.Coordinator {
+        return Coordinator()
+    }
+
+    typealias UIViewControllerType = AddCredentialsViewController
+
+    func makeUIViewController(context: Context) -> AddCredentialsView.UIViewControllerType {
+        let credentialsContext = credentialsController.credentialsContext
+        let viewController = AddCredentialsViewController(provider: provider, credentialsContext: credentialsContext)
+        viewController.onCompletion = { credentials in
+            self.presentationMode.wrappedValue.dismiss()
+        }
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: AddCredentialsView.UIViewControllerType, context: Context) {
+        // NOOP
     }
 }
 
