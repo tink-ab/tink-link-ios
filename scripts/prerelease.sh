f6b797e4619bc4a3da20a68585f76f51ca48da4e
Kasper Lahti <kasper.lahti@tink.se>
2020-10-15 12:20:28 +0200

    Update module interfaces

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -29,6 +29,9 @@ git commit -am"Format project"
 make carthage-project
 git commit -am"Update Xcode project"
 
+make module-interfaces
+git commit -am"Update module interfaces"
+
 gh pr create --repo tink-ab/tink-link-ios-private -t "rc:$newVersion" -b "Release candidate for Tink Link pre release." -r tink-ab/ios-maintainer
 
 echo Release candidate PR has been created! 🎉
