ed3e69a15cc2054d4370890f7aee30318115d3f7
Kasper Lahti <kasper.lahti@tink.se>
2020-10-15 16:26:22 +0200

    Update version tag push to tink link repo

--- scripts/postrelease.sh
+++ scripts/postrelease.sh
@@ -10,7 +10,7 @@ git pull git@github.com:tink-ab/tink-core-ios master
 
 gh pr create --repo tink-ab/tink-link-ios-private -t "Public Sync" -b "Tink Link post release public sync." -r tink-ab/ios-maintainer
 
-git push git@github.com:tink-ab/tink-core-ios-private $release
+git push git@github.com:tink-ab/tink-link-ios-private $release
 
 pod trunk push TinkCore.podspec
 
