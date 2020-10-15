072927f0592b268153de30cf10aeed64eaf8fbab
Menghao Zhang <menghao.zhang@tink.se>
2020-10-15 14:05:28 +0200

    Use default initializer

--- Tests/TinkLinkTests/Model+mock/Account+mock.swift
+++ Tests/TinkLinkTests/Model+mock/Account+mock.swift
@@ -3,14 +3,21 @@ import Foundation
 
 extension Account {
     static let checkingTestAccount = Account(
-        id: ID("f7c4038dfb6f46109ecba4a6f079a418"),
-        credentialsID: Credentials.ID("8a9255f210874f3eb5aefd78d412f6fa"),
-        name: "Checking Account tink 1",
         accountNumber: "FR1420041010050015664355590",
+        balance: 68.61,
+        credentialsID: Credentials.ID("8a9255f210874f3eb5aefd78d412f6fa"),
+        isFavored: true,
+        id: ID("f7c4038dfb6f46109ecba4a6f079a418"),
+        name: "Checking Account tink 1",
+        ownership: 1.0,
         kind: .checking,
         transferSourceIdentifiers: [URL(string: "iban://FR1420041010050015664355590?name=testAccount")!],
+        transferDestinations: nil,
+        details: nil,
         holderName: nil,
         isClosed: false,
+        flags: nil,
+        accountExclusion: nil,
         currencyDenominatedBalance: CurrencyDenominatedAmount(Decimal(68.61), currencyCode: CurrencyCode("EUR")),
         refreshed: Date(),
         financialInstitutionID: Provider.FinancialInstitution.ID("f58e31ebaf625c15a9601aa4deac83d0")
@@ -20,14 +27,21 @@ extension Account {
         credentials: Credentials
     ) -> Account {
         return Account(
-            id: Account.ID(UUID().uuidString),
-            credentialsID: credentials.id,
-            name: "Checking Account tink 1",
             accountNumber: "FR1420041010050015664355590",
+            balance: 68.61,
+            credentialsID: credentials.id,
+            isFavored: true,
+            id: Account.ID(UUID().uuidString),
+            name: "Checking Account tink 1",
+            ownership: 1.0,
             kind: .checking,
             transferSourceIdentifiers: [URL(string: "iban://FR1420041010050015664355590?name=testAccount")!],
+            transferDestinations: nil,
+            details: nil,
             holderName: nil,
             isClosed: false,
+            flags: nil,
+            accountExclusion: nil,
             currencyDenominatedBalance: CurrencyDenominatedAmount(Decimal(68.61), currencyCode: CurrencyCode("EUR")),
             refreshed: Date(),
             financialInstitutionID: Provider.FinancialInstitution.ID(UUID().uuidString)
