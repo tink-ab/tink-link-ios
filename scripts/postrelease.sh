28f30cdc0b5a6d45b776ea646b0fb1da8087c176
Kasper Lahti <kasper.lahti@tink.se>
2020-10-15 16:27:28 +0200

    Push TinkLinkUI spec also

--- scripts/postrelease.sh
+++ scripts/postrelease.sh
@@ -13,5 +13,6 @@ gh pr create --repo tink-ab/tink-link-ios-private -t "Public Sync" -b "Tink Link
 git push git@github.com:tink-ab/tink-link-ios-private $release
 
 pod trunk push TinkLink.podspec
+pod trunk push TinkLinkUI.podspec
 
 echo Tink Core public sync created and pushed to cocoapods! 🎉
