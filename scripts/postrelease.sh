b1cabd0a5a4d6765921830f04a9133b0192b522c
Kasper Lahti <kasper.lahti@tink.se>
2020-10-15 16:28:16 +0200

    Skip testing and importing for now

--- scripts/postrelease.sh
+++ scripts/postrelease.sh
@@ -12,7 +12,7 @@ gh pr create --repo tink-ab/tink-link-ios-private -t "Public Sync" -b "Tink Link
 
 git push git@github.com:tink-ab/tink-link-ios-private $release
 
-pod trunk push TinkLink.podspec
-pod trunk push TinkLinkUI.podspec
+pod trunk push TinkLink.podspec --skip-tests --skip-import-validation
+pod trunk push TinkLinkUI.podspec --skip-tests --skip-import-validation
 
 echo Tink Core public sync created and pushed to cocoapods! 🎉
