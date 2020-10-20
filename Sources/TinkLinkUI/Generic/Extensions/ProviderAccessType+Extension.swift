74dfef2bf78419c83cf755cf36f3dd828a2f0eb8
Menghao Zhang <menghao.zhang@tink.se>
2020-10-20 17:03:11 +0200

    extension for description of access type

---
+++ Sources/TinkLinkUI/Generic/Extensions/ProviderAccessType+Extension.swift
@@ -0,0 +1,12 @@
+import TinkLink
+
+extension Provider.AccessType {
+    var description: String {
+        switch self {
+        case .openBanking:
+            return "Open Banking"
+        case .other, .unknown:
+            return "Other"
+        }
+    }
+}
