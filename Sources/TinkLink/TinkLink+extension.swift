c7c7d05a2d278ae8d239320cec47fa4c9296b293
Kasper Lahti <kasper.lahti@tink.se>
2020-10-23 11:29:50 +0200

    Deprecate open method on instance and make a static version

--- Sources/TinkLink/TinkLink+extension.swift
+++ Sources/TinkLink/TinkLink+extension.swift
@@ -17,7 +17,25 @@ extension Tink {
     /// }
     /// ```
     @available(iOS 9.0, *)
+    @available(*, deprecated, message: "Use Tink.open(_:completion:) instead.")
     public func open(_ url: URL, completion: ((Result<Void, Error>) -> Void)? = nil) -> Bool {
+        Tink.open(url, completion: completion)
+    }
+
+    ///
+    /// For some providers the redirect needs to be a https link. Use the continue user activity method in your `UIApplicationDelegate` to let TinkLink send the information to Tink if needed.
+    ///
+    /// ```swift
+    /// func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    ///     if userActivity.activityType == NSUserActivityTypeBrowsingWeb, let url = userActivity.webpageURL {
+    ///         return TinkLink.shared.open(url)
+    ///     } else {
+    ///         return false
+    ///     }
+    /// }
+    /// ```
+    @available(iOS 9.0, *)
+    public static func open(_ url: URL, completion: ((Result<Void, Error>) -> Void)? = nil) -> Bool {
         guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
               urlComponents.string?.starts(with: configuration.redirectURI.absoluteString) ?? false
         else { return false }
