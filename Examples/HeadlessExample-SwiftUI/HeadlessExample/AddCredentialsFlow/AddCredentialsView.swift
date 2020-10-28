92f7706c4538cf3c4462a50c3f7c3b2619859d95
Kasper Lahti <kasper.lahti@tink.se>
2020-10-28 09:42:02 +0100

    Remove preview

--- Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/AddCredentialsView.swift
+++ Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/AddCredentialsView.swift
@@ -28,30 +28,3 @@ struct AddCredentialsView: View, UIViewControllerRepresentable {
         // NOOP
     }
 }
-
-struct AddCredentialsView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddCredentialsView(provider: .preview)
-    }
-}
-
-extension Provider {
-    static var preview = Provider(
-        id: "test",
-        displayName: "Test",
-        authenticationUserType: .personal,
-        kind: .bank,
-        status: .enabled,
-        credentialsKind: .password,
-        helpText: nil,
-        isPopular: false,
-        fields: [],
-        groupDisplayName: "Test",
-        image: nil,
-        displayDescription: "Password",
-        capabilities: [.checkingAccounts],
-        accessType: .openBanking,
-        marketCode: "SE",
-        financialInstitution: Provider.FinancialInstitution(id: "1", name: "Test")
-    )
-}
