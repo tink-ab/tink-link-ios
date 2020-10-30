7ef3a5bcf7bf9107ac4df0abd8f473a7f5958c3d
Kasper Lahti <kasper.lahti@tink.se>
2020-10-30 10:32:21 +0100

    Update example projects

--- Examples/HeadlessExample/HeadlessExample/AppDelegate.swift
+++ Examples/HeadlessExample/HeadlessExample/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        return Tink.shared.open(url)
+        return Tink.open(url)
     }
 }
 
--- Examples/TinkLinkExample/TinkLinkExample/AppDelegate.swift
+++ Examples/TinkLinkExample/TinkLinkExample/AppDelegate.swift
@@ -22,6 +22,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        return Tink.shared.open(url)
+        return Tink.open(url)
     }
 }
