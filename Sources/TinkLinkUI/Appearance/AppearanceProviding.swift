171efad7ca193cb2c54a999ad3c6195df402dea1
Kasper Lahti <kasper.lahti@tink.se>
2020-10-30 15:13:09 +0100

    Revert "Mutable appearance provider properties"

--- Sources/TinkLinkUI/Appearance/AppearanceProviding.swift
+++ Sources/TinkLinkUI/Appearance/AppearanceProviding.swift
@@ -3,17 +3,17 @@ import UIKit
 /// A type that can provide colors and fonts for Tink views.
 public protocol AppearanceProviding {
     /// Color provider
-    var colors: ColorProviding { get set }
+    var colors: ColorProviding { get }
     /// Font provider
-    var fonts: FontProviding { get set }
+    var fonts: FontProviding { get }
 }
 
 /// An appearance provider that can provide colors and fonts for Tink views.
 public struct AppearanceProvider: AppearanceProviding {
     /// Color provider
-    public var colors: ColorProviding
+    public let colors: ColorProviding
     /// Font provider
-    public var fonts: FontProviding
+    public let fonts: FontProviding
 
     /// Creates a customized Appearance with specific providers, if no value is passed, the default provider will be used.
     public init(
