94d7f6c8b0dfafa2178c215cea2c332b81bb35ee
Kasper Lahti <kasper.lahti@tink.se>
2020-11-02 16:37:01 +0100

    Rename disabled credentials error

--- Sources/TinkLink/Tasks/AddBeneficiaryTask.swift
+++ Sources/TinkLink/Tasks/AddBeneficiaryTask.swift
@@ -31,10 +31,10 @@ public final class AddBeneficiaryTask: Cancellable {
         ///
         /// The payload from the backend can be found in the associated value.
         case authenticationFailed(String)
-        /// The credentials are disabled.
+        /// The credentials are deleted.
         ///
         /// The payload from the backend can be found in the associated value.
-        case disabledCredentials(String)
+        case credentialsDeleted(String)
         /// The credentials session was expired.
         ///
         /// The payload from the backend can be found in the associated value.
@@ -245,7 +245,7 @@ extension AddBeneficiaryTask {
             }
             throw Error.authenticationFailed(payload)
         case .deleted:
-            throw Error.disabledCredentials(credentials.statusPayload ?? "")
+            throw Error.credentialsDeleted(credentials.statusPayload ?? "")
         case .sessionExpired:
             throw Error.credentialsSessionExpired(credentials.statusPayload ?? "")
         case .unknown:
