13381ec894d441236126b1c95e6e01297bfe4f3a
Kasper Lahti <kasper.lahti@tink.se>
2020-11-05 14:36:41 +0100

    Fix typo

--- Sources/TinkLink/Tasks/AuthenticationTask.swift
+++ Sources/TinkLink/Tasks/AuthenticationTask.swift
@@ -4,7 +4,7 @@
 public enum AuthenticationTask {
     /// Indicates that there is additional information required from the user to proceed.
     ///
-    /// This can for example be an OTP sent via SMS or authetication app.
+    /// This can for example be an OTP sent via SMS or authentication app.
     case awaitingSupplementalInformation(SupplementInformationTask)
     /// Indicates that there is an authentication in a third party app necessary to proceed with the authentication.
     case awaitingThirdPartyAppAuthentication(ThirdPartyAppAuthenticationTask)
