65270856d69f55ab6fc21b149f44dc8379de43dc
Kasper Lahti <kasper.lahti@tink.se>
2020-11-09 12:57:06 +0100

    Use new uri parameter name in test

--- Tests/TinkLinkTests/ThirdPartyCallbackTests.swift
+++ Tests/TinkLinkTests/ThirdPartyCallbackTests.swift
@@ -3,13 +3,13 @@ import XCTest
 
 class ThirdPartyCallbackTests: XCTestCase {
     func testValidCallbackURL() {
-        var redirectURI = Tink.shared.configuration.redirectURI
+        var redirectURI = Tink.shared.configuration.appURI!
         redirectURI.appendPathComponent("someValue")
         XCTAssert(Tink.shared.open(redirectURI))
     }
 
     func testInvalidCallbackURL() {
-        if let scheme = Tink.shared.configuration.redirectURI.scheme, let url = URL(string: "\(scheme)://randomHost/randomPath") {
+        if let scheme = Tink.shared.configuration.appURI!.scheme, let url = URL(string: "\(scheme)://randomHost/randomPath") {
             XCTAssert(!Tink.shared.open(url))
         }
     }
