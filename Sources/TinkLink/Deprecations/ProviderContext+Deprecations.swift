3ee58e6d2bdd4fe15fce724f9fceff84fb12c865
Kasper Lahti <kasper.lahti@tink.se>
2020-11-09 14:46:04 +0100

    Remove default for attributes parameter

--- Sources/TinkLink/Deprecations/ProviderContext+Deprecations.swift
+++ Sources/TinkLink/Deprecations/ProviderContext+Deprecations.swift
@@ -3,7 +3,7 @@ extension ProviderContext {
     public typealias Attributes = Filter
 
     @available(*, deprecated, renamed: "fetchProviders(filter:completion:)")
-    public func fetchProviders(attributes: Filter = .default, completion: @escaping (Result<[Provider], Error>) -> Void) -> RetryCancellable? {
+    public func fetchProviders(attributes: Filter, completion: @escaping (Result<[Provider], Error>) -> Void) -> RetryCancellable? {
         return fetchProviders(filter: attributes, completion: completion)
     }
 }
