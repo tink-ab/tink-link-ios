6ed86d667c577d1256eec6e40e0922effc8469a8
Menghao Zhang <menghao.zhang@tink.se>
2020-11-17 13:00:24 +0100

    Make content type a variable

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -3,18 +3,18 @@
 # Variables
 ENVIRONMENT=master
 SPACE_ID="tmqu5vj33f7w"
+CONTENT_TYPE="changelog"
 
 gh release view >> release.txt
 heading=$(awk -F'title:' '{print $2}'  release.txt | xargs echo -n)
 url=$(awk -F'url:' '{print $2}'  release.txt | xargs echo -n) 
 body=$(sed '1,/^--$/d' release.txt)  
-
 rm release.txt
 
 curl -X "POST" "https://api.contentful.com/spaces/${SPACE_ID}/environments/${ENVIRONMENT}/entries" --include \
 --header "Authorization: Bearer $1" \
 --header 'Content-Type: application/vnd.contentful.management.v1+json' \
---header 'X-Contentful-Content-Type: changelog' \
+--header "X-Contentful-Content-Type: ${CONTENT_TYPE}" \
 --data-binary "{
   \"fields\": {
     \"heading\": {
