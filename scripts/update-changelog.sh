f76b8b7d52b591904113b0cb0d4663dc90308129
Menghao Zhang <menghao.zhang@tink.se>
2020-11-17 13:10:12 +0100

    Clean up

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -14,17 +14,17 @@ url=$(awk -F'url:' '{print $2}'  release.txt | xargs echo -n)
 body=$(sed '1,/^--$/d' release.txt)  
 rm release.txt
 
-curl -X "POST" "https://api.contentful.com/spaces/${SPACE_ID}/environments/${ENVIRONMENT}/entries" --include \
+curl -X "POST" "https://api.contentful.com/spaces/$SPACE_ID/environments/$ENVIRONMENT/entries" --include \
 --header "Authorization: Bearer $access_token" \
 --header 'Content-Type: application/vnd.contentful.management.v1+json' \
---header "X-Contentful-Content-Type: ${CONTENT_TYPE}" \
+--header "X-Contentful-Content-Type: $CONTENT_TYPE" \
 --data-binary "{
   \"fields\": {
     \"heading\": {
-      \"en-US\": \"Released ${heading}\"
+      \"en-US\": \"Released $heading\"
     },
     \"body\": {
-      \"en-US\": \"${body}\nFor more information, see the [release notes](${url}).\"
+      \"en-US\": \"$body\nFor more information, see the [release notes]($url).\"
     },
     \"product\": {
       \"en-US\": [\"Platform\"]
