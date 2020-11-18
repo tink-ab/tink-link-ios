9d81f548ba9685b7e2deccc2a1b08d61bd36956e
Menghao Zhang <menghao.zhang@tink.se>
2020-11-18 16:54:33 +0100

    Added the download title fallback text

--- Sources/TinkLinkUI/Generic/Strings.swift
+++ Sources/TinkLinkUI/Generic/Strings.swift
@@ -70,6 +70,9 @@ enum Strings {
 
             /// Title for error shown when a temporary failure occured while adding credentials.
             static let temporaryFailure = NSLocalizedString("Credentials.Error.TemporaryFailure", tableName: tableName, bundle: bundle, value: "Temporary error", comment: "Title for error shown when a temporary failure occured while adding credentials.")
+
+            /// Title for error shown when a temporary failure occured while adding credentials.
+            static let downloadRequired = NSLocalizedString("Credentials.Error.DownloadRequired", tableName: tableName, bundle: bundle, value: "App download required", comment: "Default title for error shown when authentication app is required to be downloaded while adding credentials.")
         }
     }
 
