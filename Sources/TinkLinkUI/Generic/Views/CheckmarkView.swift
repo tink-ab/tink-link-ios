af93470947c0e39a20dd24ec792512ce31cb4a35
Kasper Lahti <kasper.lahti@tink.se>
2020-11-24 13:11:20 +0100

    Add method to animate checking of checkmark

--- Sources/TinkLinkUI/Generic/Views/CheckmarkView.swift
+++ Sources/TinkLinkUI/Generic/Views/CheckmarkView.swift
@@ -37,9 +37,63 @@ final class CheckmarkView: UIView {
         }
     }
 
-    public var isChecked: Bool = false {
-        didSet {
-            circleLayer.opacity = isChecked ? 0 : 1
+    private var _isChecked: Bool = false
+
+    var isChecked: Bool {
+        get {
+            return _isChecked
+        }
+        set {
+            _isChecked = newValue
+            circleLayer.strokeEnd = newValue ? 0 : 1
+            checkmarkLayer.strokeEnd = newValue ? 0 : 1
+        }
+    }
+
+    func setChecked(_ checked: Bool, animated: Bool) {
+        if animated {
+            if checked {
+                let circleAnimation = CABasicAnimation(keyPath: "strokeEnd")
+                circleAnimation.duration = 0.3
+                circleAnimation.fromValue = 0
+                circleAnimation.toValue = 1
+                circleAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+                circleLayer.add(circleAnimation, forKey: "CheckAnimation")
+                circleLayer.strokeEnd = 1
+
+                let keyframeAnimation = CAKeyframeAnimation(keyPath: "strokeEnd")
+                keyframeAnimation.duration = 1.0
+                keyframeAnimation.values = [0.0, 0.0, 0.33, 0.33, 1.0]
+                keyframeAnimation.keyTimes = [0.0, 0.4, 0.6, 0.7, 1.0]
+                keyframeAnimation.timingFunctions = [
+                    CAMediaTimingFunction(name: .linear),
+                    CAMediaTimingFunction(name: .easeOut),
+                    CAMediaTimingFunction(name: .linear),
+                    CAMediaTimingFunction(name: .easeOut)
+                ]
+                checkmarkLayer.add(keyframeAnimation, forKey: "CheckAnimation")
+
+                checkmarkLayer.strokeEnd = 1
+            } else {
+                let circleAnimation = CABasicAnimation(keyPath: "strokeEnd")
+                circleAnimation.duration = 0.4
+                circleAnimation.fromValue = 1
+                circleAnimation.toValue = 0
+                circleAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+                circleLayer.add(circleAnimation, forKey: "UncheckAnimation")
+                circleLayer.strokeEnd = 0
+
+                let animation = CABasicAnimation(keyPath: "strokeEnd")
+                animation.duration = 0.4
+                animation.fromValue = 1
+                animation.toValue = 0
+                animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+                checkmarkLayer.add(animation, forKey: "UncheckAnimation")
+                checkmarkLayer.strokeEnd = 0
+            }
+            _isChecked = checked
+        } else {
+            isChecked = checked
         }
     }
 
@@ -67,12 +121,16 @@ final class CheckmarkView: UIView {
 
     private func setup() {
         circleLayer.actions = [
-            "opacity": NSNull()
+            "strokeEnd": NSNull()
+        ]
+        checkmarkLayer.actions = [
+            "strokeEnd": NSNull()
         ]
 
         checkmarkLayer.fillColor = UIColor.clear.cgColor
         checkmarkLayer.lineWidth = style.lineWidth
         checkmarkLayer.strokeColor = tintColor.cgColor
+        checkmarkLayer.strokeEnd = isChecked ? 1 : 0
         layer.addSublayer(checkmarkLayer)
 
         setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
@@ -85,6 +143,7 @@ final class CheckmarkView: UIView {
         circleLayer.fillColor = UIColor.clear.cgColor
         circleLayer.strokeColor = tintColor.cgColor
         circleLayer.lineWidth = style.lineWidth
+        circleLayer.setAffineTransform(CGAffineTransform(rotationAngle: -(.pi / 2)))
         layer.addSublayer(circleLayer)
     }
 
