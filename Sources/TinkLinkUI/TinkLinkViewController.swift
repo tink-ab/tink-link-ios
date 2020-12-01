cd815b97d5fabd2b21638f2b7a1bb5406d53c49b
Kasper Lahti <kasper.lahti@tink.se>
2020-12-01 11:50:32 +0100

    Add a new transition for form to loading screen

--- Sources/TinkLinkUI/TinkLinkViewController.swift
+++ Sources/TinkLinkUI/TinkLinkViewController.swift
@@ -709,7 +709,7 @@ extension TinkLinkViewController: CredentialsCoordinatorDelegate {
 extension TinkLinkViewController: UINavigationControllerDelegate {
     public func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         if operation == .push, fromVC is CredentialsFormViewController, toVC is LoadingViewController {
-            return CredentialsSuccessfullyAddedTransition()
+            return CredentialsFormToLoadingTransition()
         } else if operation == .push, fromVC is LoadingViewController, toVC is CredentialsSuccessfullyAddedViewController {
             return CredentialsSuccessfullyAddedTransition()
         } else {
