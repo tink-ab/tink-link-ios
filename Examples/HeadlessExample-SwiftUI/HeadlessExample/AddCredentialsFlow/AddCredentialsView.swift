66498810661ee83488795b600f4b6e499dab60db
Kasper Lahti <kasper.lahti@tink.se>
2021-01-05 09:51:50 +0100

    Change add credentials view to not be a uiview representable

--- Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/AddCredentialsView.swift
+++ Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/AddCredentialsView.swift
@@ -1,30 +1,13 @@
 import SwiftUI
 import TinkLink
 
-struct AddCredentialsView: View, UIViewControllerRepresentable {
+struct AddCredentialsView: View {
     var provider: Provider
 
     @EnvironmentObject var credentialsController: CredentialsController
     @SwiftUI.Environment(\.presentationMode) var presentationMode
 
-    class Coordinator {}
-
-    func makeCoordinator() -> AddCredentialsView.Coordinator {
-        return Coordinator()
-    }
-
-    typealias UIViewControllerType = AddCredentialsViewController
-
-    func makeUIViewController(context: Context) -> AddCredentialsView.UIViewControllerType {
-        let credentialsContext = credentialsController.credentialsContext
-        let viewController = AddCredentialsViewController(provider: provider, credentialsContext: credentialsContext)
-        viewController.onCompletion = { credentials in
-            self.presentationMode.wrappedValue.dismiss()
-        }
-        return viewController
-    }
-
-    func updateUIViewController(_ uiViewController: AddCredentialsView.UIViewControllerType, context: Context) {
-        // NOOP
+    var body: some View {
+        EmptyView()
     }
 }
