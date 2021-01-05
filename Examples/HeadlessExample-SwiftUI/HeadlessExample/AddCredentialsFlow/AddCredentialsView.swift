be872622f0d88207dbcfa41f48468e702f7653de
Kasper Lahti <kasper.lahti@tink.se>
2021-01-05 09:53:55 +0100

    Setup form state

--- Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/AddCredentialsView.swift
+++ Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/AddCredentialsView.swift
@@ -3,9 +3,15 @@ import TinkLink
 
 struct AddCredentialsView: View {
     var provider: Provider
+    @State private var form: TinkLink.Form
 
     @EnvironmentObject var credentialsController: CredentialsController
     @SwiftUI.Environment(\.presentationMode) var presentationMode
+
+    init(provider: Provider) {
+        self.provider = provider
+        self._form = State(initialValue: TinkLink.Form(provider: provider))
+    }
 
     var body: some View {
         EmptyView()
