5b8ccb45c3425cf89a25c8bc22499e253a089e9b
Kasper Lahti <kasper.lahti@tink.se>
2021-01-05 11:37:42 +0100

    Setup form state

--- Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/SupplementalInformationForm.swift
+++ Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/SupplementalInformationForm.swift
@@ -4,12 +4,15 @@ import TinkLink
 struct SupplementalInformationForm: View {
     private var supplementInformationTask: SupplementInformationTask
 
+    @State private var form: TinkLink.Form
+
     typealias CompletionHandler = (Result<Void, Error>) -> Void
     var onCompletion: CompletionHandler
 
     init(supplementInformationTask: SupplementInformationTask, onCompletion: @escaping CompletionHandler) {
         self.supplementInformationTask = supplementInformationTask
         self.onCompletion = onCompletion
+        self._form = State(initialValue: TinkLink.Form(supplementInformationTask: supplementInformationTask))
     }
 
     var body: some View {
