7fbc55c2f100407cd603d862c6b7fe8478d96208
Kasper Lahti <kasper.lahti@tink.se>
2021-01-05 11:22:14 +0100

    Setup form state

--- Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/UpdateCredentialsFlowView.swift
+++ Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/UpdateCredentialsFlowView.swift
@@ -8,12 +8,15 @@ struct UpdateCredentialsFlowView: View {
     private let provider: Provider
     private let credentials: Credentials
 
+    @State private var form: TinkLink.Form
+
     @EnvironmentObject var credentialsController: CredentialsController
 
     init(provider: Provider, credentials: Credentials, onCompletion: @escaping CompletionHandler) {
         self.onCompletion = onCompletion
         self.provider = provider
         self.credentials = credentials
+        self._form = State(initialValue: TinkLink.Form(updatingCredentials: credentials, provider: provider))
     }
 
     var body: some View {
