c97d30d6059f8ec8ef2b417bf920b2bd3a9d81b6
Kasper Lahti <kasper.lahti@tink.se>
2021-01-05 11:36:32 +0100

    Change supplemental information form view to not be a uiview representable

--- Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/SupplementalInformationForm.swift
+++ Examples/HeadlessExample-SwiftUI/HeadlessExample/AddCredentialsFlow/SupplementalInformationForm.swift
@@ -1,40 +1,18 @@
 import SwiftUI
 import TinkLink
 
-struct SupplementalInformationForm: View, UIViewControllerRepresentable {
+struct SupplementalInformationForm: View {
     private var supplementInformationTask: SupplementInformationTask
+
+    typealias CompletionHandler = (Result<Void, Error>) -> Void
+    var onCompletion: CompletionHandler
 
     init(supplementInformationTask: SupplementInformationTask, onCompletion: @escaping CompletionHandler) {
         self.supplementInformationTask = supplementInformationTask
         self.onCompletion = onCompletion
     }
 
-    class Coordinator {
-        let completionHandler: CompletionHandler
-
-        init(completionHandler: @escaping CompletionHandler) {
-            self.completionHandler = completionHandler
-        }
-    }
-
-    typealias CompletionHandler = (Result<Void, Error>) -> Void
-    var onCompletion: CompletionHandler
-
-    typealias UIViewControllerType = UINavigationController
-
-    func makeCoordinator() -> SupplementalInformationForm.Coordinator {
-        return Coordinator(completionHandler: onCompletion)
-    }
-
-    func makeUIViewController(context: UIViewControllerRepresentableContext<SupplementalInformationForm>) -> SupplementalInformationForm.UIViewControllerType {
-        let viewController = SupplementalInformationViewController(supplementInformationTask: supplementInformationTask)
-        viewController.onCompletion = context.coordinator.completionHandler
-        let navigationController = UINavigationController(rootViewController: viewController)
-        navigationController.navigationBar.prefersLargeTitles = true
-        return navigationController
-    }
-
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: UIViewControllerRepresentableContext<SupplementalInformationForm>) {
-        // NOOP
+    var body: some View {
+        EmptyView()
     }
 }
