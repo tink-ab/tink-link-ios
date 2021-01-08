4426a188320d6a318d50f86123e01e80df251450
Kasper Lahti <kasper.lahti@tink.se>
2021-01-08 09:27:34 +0100

    Add small caps helper

--- Sources/TinkLinkUI/Generic/Extensions/UIFont+Bold.swift
+++ Sources/TinkLinkUI/Generic/Extensions/UIFont+Bold.swift
@@ -7,4 +7,17 @@ extension UIFont {
         }
         return UIFont(descriptor: boldFontDescriptor, size: pointSize)
     }
+
+    var smallCaps: UIFont {
+        let lowerCaseFeatureSetting: [UIFontDescriptor.FeatureKey: Any] = [
+            .featureIdentifier: kLowerCaseType,
+            .typeIdentifier: kLowerCaseSmallCapsSelector
+        ]
+        let upperCaseFeatureSetting: [UIFontDescriptor.FeatureKey: Any] = [
+            .featureIdentifier: kUpperCaseType,
+            .typeIdentifier: kUpperCaseSmallCapsSelector
+        ]
+        let smallCapsFontDescriptor = fontDescriptor.addingAttributes([.featureSettings: [lowerCaseFeatureSetting, upperCaseFeatureSetting]])
+        return UIFont(descriptor: smallCapsFontDescriptor, size: pointSize)
+    }
 }
