88b6948c185bcd894f57289c2113e3d2b02bf6b4
Kasper Lahti <kasper.lahti@tink.se>
2021-03-03 11:05:01 +0100

    Use source files

--- scripts/update-core-for-development.sh
+++ scripts/update-core-for-development.sh
@@ -4,3 +4,11 @@
 old_path=".package(name: \"TinkCore\", url: \"https://github.com/tink-ab/tink-core-ios\", .upToNextMinor(from: \"0.7.0\")),"
 new_path=".package(name: \"TinkCore\", path: \"../tink-core-ios\"),"
 sed -i '' "s|$old_path|$new_path|" Package.swift
+
+# Use source files for core package
+cd ../tink-core-ios
+
+sed -i '' '19,22d' Package.swift
+sed -i '' 's/targets: \[/targets: \[.target(name: \"TinkCore\", exclude: \[\"Info.plist\"\])/' Package.swift
+
+cd ../tink-link-ios
