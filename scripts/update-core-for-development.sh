5f7ce4706f98685cba9b52f617f6bb658edd9795
Kasper Lahti <kasper.lahti@tink.se>
2021-03-03 11:04:55 +0100

    Fix path

--- scripts/update-core-for-development.sh
+++ scripts/update-core-for-development.sh
@@ -2,5 +2,5 @@
 
 # Use local core dependency
 old_path=".package(name: \"TinkCore\", url: \"https://github.com/tink-ab/tink-core-ios\", .upToNextMinor(from: \"0.7.0\")),"
-new_path=".package(name: \"TinkCore\", path: \"../tink-core-ios-private\"),"
+new_path=".package(name: \"TinkCore\", path: \"../tink-core-ios\"),"
 sed -i '' "s|$old_path|$new_path|" Package.swift
