1921cb78ef7258a733bab33da386ee889775fedc
Menghao Zhang <menghao.zhang@tink.se>
2021-03-05 13:38:23 +0100

    update test for core 1.0

--- scripts/updateCoreForTest.sh
+++ scripts/updateCoreForTest.sh
@@ -4,13 +4,13 @@ rm -rf ./tink-core-ios-private
 
 git clone -v git@github.com:tink-ab/tink-core-ios-private.git
 cd ./tink-core-ios-private
-git checkout tags/1.0.0-rc.2
+git checkout tags/1.0.0
 make internal-distribution-framework
 rm -rf TinkCore.xcframework
 cp -r ./build/TinkCore-internal.xcframework TinkCore.xcframework
 sed -i '' "s/url: \".*\.xcframework\.zip\", checksum: \".*\"/path: \"TinkCore\.xcframework\"/" Package.swift
 cd ..
 
-old_path=".package(name: \"TinkCore\", url: \"https://github.com/tink-ab/tink-core-ios\", .exact(\"1.0.0-rc.2\")),"
+old_path=".package(name: \"TinkCore\", url: \"https://github.com/tink-ab/tink-core-ios\", .exact(\"1.0.0\")),"
 new_path=".package(name: \"TinkCore\", path: \"./tink-core-ios-private\"),"
 sed -i '' "s|$old_path|$new_path|" Package.swift
