7c93f45d11482da18a50deb61b025a803dd9487a
Menghao Zhang <menghao.zhang@tink.se>
2021-03-05 14:32:28 +0100

    Add the script for update the project yml

--- scripts/updateCoreForTest.sh
+++ scripts/updateCoreForTest.sh
@@ -14,3 +14,9 @@ cd ..
 old_path=".package(name: \"TinkCore\", url: \"https://github.com/tink-ab/tink-core-ios\", .exact(\"1.0.0\")),"
 new_path=".package(name: \"TinkCore\", path: \"./tink-core-ios-private\"),"
 sed -i '' "s|$old_path|$new_path|" Package.swift
+
+
+sed -i '' '108d' project.yml
+old_core_path="url: https://github.com/tink-ab/tink-core-ios"
+new_core_path="path: ./tink-core-ios-private"
+sed -i '' "s|$old_core_path|$new_core_path|" project.yml
