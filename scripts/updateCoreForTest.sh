71cd2be881ff62a9148e61f9bf1338de09f71efc
Menghao Zhang <menghao.zhang@tink.se>
2021-03-08 09:14:59 +0100

    test script fix

--- scripts/updateCoreForTest.sh
+++ scripts/updateCoreForTest.sh
@@ -11,7 +11,7 @@ cp -r ./build/TinkCore-internal.xcframework TinkCore.xcframework
 sed -i '' "s/url: \".*\.xcframework\.zip\", checksum: \".*\"/path: \"TinkCore\.xcframework\"/" Package.swift
 cd ..
 
-old_path=".package(name: \"TinkCore\", url: \"https://github.com/tink-ab/tink-core-ios\", .exact(\"1.0.0\")),"
+old_path=".package(name: \"TinkCore\", url: \"https://github.com/tink-ab/tink-core-ios\", .upToNextMinor(\"1.0.0\")),"
 new_path=".package(name: \"TinkCore\", path: \"./tink-core-ios-private\"),"
 sed -i '' "s|$old_path|$new_path|" Package.swift
 
