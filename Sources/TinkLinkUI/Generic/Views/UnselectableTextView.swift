72af94a05a1b2d31eec1040a2d9dfc38ff12d747
Menghao Zhang <menghao.zhang@tink.se>
2021-05-06 20:48:32 +0800

    Override the gestureRecognizer

--- Sources/TinkLinkUI/Generic/Views/UnselectableTextView.swift
+++ Sources/TinkLinkUI/Generic/Views/UnselectableTextView.swift
@@ -7,4 +7,26 @@ final class UnselectableTextView: UITextView {
         get { return nil }
         set {}
     }
+
+    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer is UIPanGestureRecognizer {
+            // required for compatibility with isScrollEnabled
+            return super.gestureRecognizerShouldBegin(gestureRecognizer)
+        }
+        if let tapGestureRecognizer = gestureRecognizer as? UITapGestureRecognizer,
+           tapGestureRecognizer.numberOfTapsRequired == 1 {
+            // required for compatibility with links
+            return super.gestureRecognizerShouldBegin(gestureRecognizer)
+        }
+        // allowing smallDelayRecognizer for links
+        // https://stackoverflow.com/questions/46143868/xcode-9-uitextview-links-no-longer-clickable
+        if let longPressGestureRecognizer = gestureRecognizer as? UILongPressGestureRecognizer,
+           // comparison value is used to distinguish between 0.12 (smallDelayRecognizer) and 0.5 (textSelectionForce and textLoupe)
+           longPressGestureRecognizer.minimumPressDuration < 0.325 {
+            return super.gestureRecognizerShouldBegin(gestureRecognizer)
+        }
+        // preventing selection from loupe/magnifier (_UITextSelectionForceGesture), multi tap, tap and a half, etc.
+        gestureRecognizer.isEnabled = false
+        return false
+    }
 }
