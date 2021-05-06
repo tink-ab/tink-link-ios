14264b27a6f71d3a7f708b7f3821efef9347425d
Menghao Zhang <menghao.zhang@tink.se>
2021-05-06 20:20:56 +0800

    Make the text not selectable but also support hover

--- Sources/TinkLinkUI/Generic/Views/UnselectableTextView.swift
+++ Sources/TinkLinkUI/Generic/Views/UnselectableTextView.swift
@@ -1,15 +1,12 @@
 import UIKit
 
-// Taken from https://stackoverflow.com/a/44878203
+// Taken from https://stackoverflow.com/a/50369557
+// Make the text not selectable but also support voice over
 final class UnselectableTextView: UITextView {
-    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
-        guard
-            let position = closestPosition(to: point),
-            let range = tokenizer.rangeEnclosingPosition(position, with: .character, inDirection: .layout(.left))
-        else { return false }
-
-        let startIndex = offset(from: beginningOfDocument, to: range.start)
-
-        return attributedText.attribute(.link, at: startIndex, effectiveRange: nil) != nil
+    override public var selectedTextRange: UITextRange? {
+        get {
+            return nil
+        }
+        set { }
     }
 }
