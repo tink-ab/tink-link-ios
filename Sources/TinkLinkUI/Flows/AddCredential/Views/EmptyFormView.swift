c3d52cf139b27f37c330e21d93d3952346fe9648
Menghao Zhang <menghao.zhang@tink.se>
2021-06-14 17:02:39 +0200

    Update the constraint priority

--- Sources/TinkLinkUI/Flows/AddCredential/Views/EmptyFormView.swift
+++ Sources/TinkLinkUI/Flows/AddCredential/Views/EmptyFormView.swift
@@ -150,9 +150,9 @@ final class EmptyFormView: UIView {
             instructionLabel.trailingAnchor.constraint(equalTo: instructionView.trailingAnchor, constant: -24),
             instructionLabel.bottomAnchor.constraint(equalTo: instructionView.bottomAnchor, constant: -24),
 
-            instructionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 34).withPriority(.defaultHigh),
+            instructionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 34).withPriority(UILayoutPriority(751)),
             instructionView.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 34),
-            instructionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -34).withPriority(.defaultHigh),
+            instructionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -34).withPriority(UILayoutPriority(751)),
             instructionView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             instructionView.widthAnchor.constraint(lessThanOrEqualToConstant: 460),
             contentViewCenterYConstraint,
