bea84018ea7ef8f163400ff382a568e8c528c695
Vitalii Kuznetsov <vkuznets@visa.com>
2025-05-06 13:52:41 +0200

    Update sample app

--- Samples/TinkLinkSimpleSample/TinkLinkSimpleSample/SceneDelegate.swift
+++ Samples/TinkLinkSimpleSample/TinkLinkSimpleSample/SceneDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import TinkLink
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -6,5 +7,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard (scene as? UIWindowScene) != nil else { return }
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts urlContexts: Set<UIOpenURLContext>) {
+        guard let url = urlContexts.first?.url else { return }
+            
+        Tink.openUrl(url)
     }
 }
